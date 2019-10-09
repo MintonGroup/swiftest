77f817449b437174cf2b2833e5ad7f8f82516eea
Jennifer <jpouplin@purdue.edu>
2019-10-09 11:41:09 -0400

    create symba_fragmentation file
    added function call to main + created function

---
+++ symba/symba_fragmentation.f90
@@ -0,0 +1,75 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : symba_fragmentation
+!  Unit Type   : subroutine
+!  Project     : Swifter
+!  Package     : symba
+!  Language    : Fortran 90/95
+!
+!  Description : Merge planets
+!
+!  Input
+!    Arguments : t            : time
+!                npl          : number of planets
+!                nsppl        : number of spilled planets
+!                symba_pl1P   : pointer to head of SyMBA planet structure linked-list
+!                symba_pld1P  : pointer to head of discard SyMBA planet structure linked-list
+!                nplplenc     : number of planet-planet encounters
+!                plplenc_list : array of planet-planet encounter structures
+!    Terminal  : none
+!    File      : none
+!
+!  Output
+!    Arguments : npl          : number of planets
+!                nsppl        : number of spilled planets
+!                symba_pl1P   : pointer to head of SyMBA planet structure linked-list
+!                symba_pld1P  : pointer to head of discard SyMBA planet structure linked-list
+!    Terminal  : none
+!    File      : none
+!
+!  Invocation  : CALL symba_fragmentation(t, npl, nsppl, symba_pl1P, symba_pld1P, nplplenc, plplenc_list)
+!
+!  Notes       : Adapted from Hal Levison's Swift routine discard_mass_merge.f
+!
+!**********************************************************************************************************************************
+SUBROUTINE symba_fragmentation (t, npl, nplmax, ntp, ntpmax, symba_pl1P, nplplenc, plplenc_list)
+
+! Modules
+     USE module_parameters
+     USE module_swifter
+     USE module_helio
+     USE module_symba
+     USE module_interfaces, EXCEPT_THIS_ONE => symba_fragmentation
+     IMPLICIT NONE
+
+! Arguments
+     INTEGER(I4B), INTENT(IN)                      :: nplplenc
+     INTEGER(I4B), INTENT(INOUT)                   :: npl, nplmax, ntp, ntpmax
+     REAL(DP), INTENT(IN)                          :: t
+     TYPE(symba_pl), POINTER                       :: symba_pl1P
+     TYPE(symba_plplenc), DIMENSION(:), INTENT(IN) :: plplenc_list
+
+! Internals
+ 
+! Executable code
+     RETURN
+
+END SUBROUTINE symba_fragmentation
+!**********************************************************************************************************************************
+!
+!  Author(s)   : David E. Kaufmann
+!
+!  Revision Control System (RCS) Information
+!
+!  Source File : $RCSfile$
+!  Full Path   : $Source$
+!  Revision    : $Revision$
+!  Date        : $Date$
+!  Programmer  : $Author$
+!  Locked By   : $Locker$
+!  State       : $State$
+!
+!  Modification History:
+!
+!  $Log$
+!**********************************************************************************************************************************
