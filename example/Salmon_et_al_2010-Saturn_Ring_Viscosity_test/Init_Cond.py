d2bf5951088e5501ffc93d4d381c83ceb7e5da6b
David Minton <daminton@purdue.edu>
2019-10-19 09:52:04 -0400

    Updated Salmon et al. 2010 example with matching initial conditions and plots

--- example/Salmon_et_al_2010-Saturn_Ring_Viscosity_test/Init_Cond.py
+++ example/Salmon_et_al_2010-Saturn_Ring_Viscosity_test/Init_Cond.py
@@ -1,5 +1,8 @@
+from typing import List, Any
+
 import numpy as np
 import sys
+
 
 ###***Define initial conditions***###
 
@@ -10,7 +13,7 @@ AU      = 1.4960e13
 M_Sun   = 1.9891e33
 
 
-N   = 175            #number of bins in disk
+N   = 1000           #number of bins in disk
 
 #M_Uranus =  8.6810e28 # mass of Uranus in gm
 #R_Uranus = 25362e5 # Radius of Uranus in cm
@@ -22,65 +25,63 @@ N   = 175            #number of bins in disk
 
 M_Saturn    = 5.6846e29
 R_Saturn    = 60268.0e5
+R_Planet = R_Saturn
+M_Planet = M_Saturn
 a_Saturn    = 9.582172*AU
 T_Saturn    = 10.57*3600
-W_Saturn    = 2.0*pi/T_Saturn
+W_Saturn    = 2.0*np.pi/T_Saturn
 L_Saturn    = 2.0*M_Saturn*R_Saturn**2.0/5.0*W_Saturn
 Rhill_Saturn = a_Saturn * (M_Saturn / (3 * M_Sun))**(1.0 / 3.0)
-
 
 k_2         = 0.104 #tidal love number for primary
 Q           = 3000. #tidal dissipation factor for primary
 Q_s         = 1.0e-5    #tidal dissipation factor for satellites
 
-rho_sat = 1.2 # Satellite/ring particle mass density in gm/cm**3
+rho_sat = 1.0 # Satellite/ring particle mass density in gm/cm**3
 
 J2 = 0.0 #Add these in later
 J4 = 0.0
 
 #The following are Unit conversion factors
-MU2GM    =      M_Uranus          #Conversion from mass unit to grams
-DU2CM    =      R_Uranus                       #Conversion from radius unit to centimeters
+MU2GM    =      M_Saturn          #Conversion from mass unit to grams
+DU2CM    =      R_Saturn                       #Conversion from radius unit to centimeters
 TU2S     =      year                           #Conversion from time unit to seconds
 GU       = G / (DU2CM**3 / (MU2GM * TU2S**2))
-
 
 #Primary body definitions
 RP    = 1.0
 MP    = 1.0
 rhoP  = 3.0 * MP / (4.0 * np.pi * RP**3) #Density of primary
-TP    =  T_Uranus / TU2S
+TP    =  T_Saturn / TU2S
 IP = 2.0 / 5.0 * MP * RP**2
 wP = np.array([0.0,0.0,1.0]) * 2.0 * np.pi / TP
 LP =  IP * wP
 
 
-
-
-
-
-
-
 ###For the disk:
 
-r_p100m = 100e2                        #radius of 100 m planetesimal (cm)
-m_p100m = (4.0 * np.pi * r_p100m**3.0) / 3.0 * rho_sat     #mass of 100 m planetesimal (g)
 
-r_pdisk = r_p100m    #disk particle size (radius)
-m_pdisk = m_p100m  #disk particle size (mass)
+
+r_pdisk = 13.5    #disk particle size (radius)
+m_pdisk = (4.0 * np.pi*r_pdisk**3)/3.0 * rho_sat   #disk particle size (mass)
 
 # gamma	= 0.3	    #ang momentum efficiency factor
 inside = 0  #bin id of innermost ring bin (can increase if primary accretes a lot mass through 'Update.py'
 p	=   3   #Power law index that surface mass density falls off as (cannot be exactly 2)
-alpha	=  4.12e32      #Arbitrary constant (sets initial mass of disk in "build.py"
-r_I	= R_Uranus     #inside radius of disk is at the embryo's surface
-r_F	= 5.0 * R_Uranus  #outside radius of disk
+alpha	=  1e30      #Arbitrary constant (sets initial mass of disk in "build.py"
+r_I	= 100e8      #inside radius of disk is at the embryo's surface
+r_F	= 120e8  #outside radius of disk
 deltar = (r_F - r_I) / N	#width of a bin
 deltaX = (2. * r_F**0.5 - 2.*r_I**0.5) / N  #variable changed bin width used for viscosity calculations
 r = []
 deltaA = []
 m = []
 sigma = []
+X = []
+w = []
+I = []
+R = []
+Torque_to_disk = []
 
 def f(x):
     #Creates initial values for the disk and prints them out
@@ -94,13 +95,18 @@ def f(x):
             sigma.append(m[a]/deltaA[a])
         #Gaussian surface mass density profile
         elif x == 1:
-            centroid = 20       #bin id of the center of the gaussian
-            spread = 1          #width of the gaussian
-            mass_scale = 2.3e6  #scale factor to get a given mass
+            centroid = 501    #bin id of the center of the gaussian
+            spread = 18.0      #width of the gaussian
+            mass_scale = 7.6e4 #scale factor to get a given mass
             sigma.append(mass_scale/(5*(2*np.pi)**(0.5))*np.exp(-(a-centroid)**2/(2*spread**2)))
             m.append(sigma[a]*deltaA[a])
+            X.append(2.*r[a]**0.5)
         else:
             print('You have not chosen a valid disk model')
+        R.append(r[a]**2 + deltar**2/4)
+        I.append(m[a]*R[a])
+        w.append((G*M_Planet/r[a]**3)**0.5)
+        Torque_to_disk.append(0.0)
 
 f(1) #Make a Gaussian ring
 
@@ -141,7 +147,7 @@ tpfile.close()
 
 iout = int(np.ceil(end_sim / (deltaT * t_print)))
 rmin = RP
-rmax = Rhill_Uranus / DU2CM
+rmax = Rhill_Saturn / DU2CM
 
 mtiny = 1e-8 #roughly 1/3 the mass of Puck
 
@@ -182,5 +188,5 @@ print(f'TU2S           {TU2S}')
 print(f'MTINY          {mtiny}')
 
 
-
+sys.stdout = sys.__stdout__
 
--- example/Salmon_et_al_2010-Saturn_Ring_Viscosity_test/plot_sigma_evolution.py
+++ example/Salmon_et_al_2010-Saturn_Ring_Viscosity_test/plot_sigma_evolution.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import Init_Cond as ic
+import Visc5
 
 
 figure = plt.figure(1, figsize=(8,6))    #this should be called later when we know the aspect ratio, but for now, I have it here
@@ -23,18 +25,43 @@ S2010test_t0 = np.loadtxt('S2010test-t0.csv',delimiter=',')
 S2010test_t1e3 = np.loadtxt('S2010test-t1e3.csv',delimiter=',')
 S2010test_t1e4 = np.loadtxt('S2010test-t1e4.csv',delimiter=',')
 S2010test_t1e5 = np.loadtxt('S2010test-t1e5.csv',delimiter=',')
-axes['a'].plot(S2010test_t0[:,0]*1e-3, S2010test_t0[:,1]*1e-4, '-', color="black", linewidth=1.0, zorder=40)
-axes['b'].plot(S2010test_t1e3[:,0]*1e-3, S2010test_t1e3[:,1]*1e-4, '-', color="black", linewidth=1.0, zorder=40)
-axes['c'].plot(S2010test_t1e4[:,0]*1e-3, S2010test_t1e4[:,1]*1e-4, '-', color="black", linewidth=1.0, zorder=40)
-axes['d'].plot(S2010test_t1e5[:,0]*1e-3, S2010test_t1e5[:,1]*1e-4, '-', color="black", linewidth=1.0, zorder=40)
+axes['a'].plot(S2010test_t0[:,0]*1e-3, S2010test_t0[:,1]*1e-4, '-', color="red", linewidth=1.0, zorder=40)
+axes['b'].plot(S2010test_t1e3[:,0]*1e-3, S2010test_t1e3[:,1]*1e-4, '-', color="red", linewidth=1.0, zorder=40)
+axes['c'].plot(S2010test_t1e4[:,0]*1e-3, S2010test_t1e4[:,1]*1e-4, '-', color="red", linewidth=1.0, zorder=40)
+axes['d'].plot(S2010test_t1e5[:,0]*1e-3, S2010test_t1e5[:,1]*1e-4, '-', color="red", linewidth=1.0, zorder=40)
 
 axes['a'].title.set_text('$0$ years')
 axes['b'].title.set_text('$10^3$ years')
 axes['c'].title.set_text('$10^4$ years')
 axes['d'].title.set_text('$10^5$ years')
 
-figure.tight_layout()
-plt.show()
+r = np.asarray(ic.r) * 1e-2 * 1e-3 * 1e-3 #cm to 1000 km
+sigma = np.asarray(ic.sigma) * 1e-3 * 1e4 * 1e-4 #g/cm**2 to 1e4 kg/m**2
+axes['a'].plot(r, sigma, '-', color="black", linewidth=1.0, zorder = 50)
 
+#ringfile = 'ring.in'
+#with open(ringfile) as f:
+   #N = int(f.readline())
+  # sigma = np.empty(N,dtype=np.float64)
+  # deltar=f.readline()
+  # vals = [float(r) for r in f.readline().split()]
+  # r_pdisk = vals[0]
+  # Gm_pdisk = vals[1]
+  # for i, line in enumerate(f):
+  #     sigma[i] = line
+#
+#print(Gm_pdisk)
+
+Visc5.f(1,ic.M_Saturn,t=0.0)
+print(f"nu = {Visc5.nu[501]*1e-2}")
+
+figure.tight_layout()
+#plt.show()
+
+
+
+#figname ="Salmon_et_al_2010-Saturn_Ring_Viscocity_evoloution.png"
+#plt.savefig(figname,dpi=300 )
+#os.system(f'open {figname}')
 
 
