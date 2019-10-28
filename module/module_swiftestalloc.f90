5d2cf15391ede3f600d7466b59b5b1065e27ea57
Jennifer <jpouplin@purdue.edu>
2019-10-28 11:31:46 -0400

    edited deallocate

--- module/module_swiftestalloc.f90
+++ module/module_swiftestalloc.f90
@@ -204,169 +204,160 @@ MODULE module_swiftestalloc
 !___________________________
 
 
-        SUBROUTINE swiftest_pl_deallocate(swiftest_plA, npl)
+        SUBROUTINE swiftest_pl_deallocate(swiftest_plA)
 			USE module_parameters
 			USE module_swiftest
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)			:: npl
 			TYPE(swiftest_pl), INTENT(INOUT)	:: swiftest_plA
 
-			DEALLOCATE(swiftest_plA%id(npl))
-         	DEALLOCATE(swiftest_plA%status(npl))
-         	DEALLOCATE(swiftest_plA%mass(npl))
-         	DEALLOCATE(swiftest_plA%radius(npl))
-         	DEALLOCATE(swiftest_plA%rhill(npl))
-         	DEALLOCATE(swiftest_plA%xh(NDIM,npl))
-         	DEALLOCATE(swiftest_plA%vh(NDIM,npl))
-         	DEALLOCATE(swiftest_plA%xb(NDIM,npl))
-         	DEALLOCATE(swiftest_plA%vb(NDIM,npl))
+			DEALLOCATE(swiftest_plA%id)
+         	DEALLOCATE(swiftest_plA%status)
+         	DEALLOCATE(swiftest_plA%mass)
+         	DEALLOCATE(swiftest_plA%radius)
+         	DEALLOCATE(swiftest_plA%rhill)
+         	DEALLOCATE(swiftest_plA%xh)
+         	DEALLOCATE(swiftest_plA%vh)
+         	DEALLOCATE(swiftest_plA%xb)
+         	DEALLOCATE(swiftest_plA%vb)
         	return
         END SUBROUTINE swiftest_pl_deallocate
 
 
-        SUBROUTINE helio_pl_deallocate(helio_plA, npl)
+        SUBROUTINE helio_pl_deallocate(helio_plA)
 			USE module_parameters
 			USE module_helio
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)			:: npl
 			TYPE(helio_pl), INTENT(INOUT)		:: helio_plA
 
-			DEALLOCATE(helio_plA%ah(NDIM,npl))
-         	DEALLOCATE(helio_plA%ahi(NDIM,npl))
-         	CALL swiftest_pl_deallocate(helio_plA%swiftest,npl)
+			DEALLOCATE(helio_plA%ah)
+         	DEALLOCATE(helio_plA%ahi)
+         	CALL swiftest_pl_deallocate(helio_plA%swiftest)
         	return
         END SUBROUTINE helio_pl_deallocate
 
 
-        SUBROUTINE symba_pl_deallocate(symba_plA, npl)
+        SUBROUTINE symba_pl_deallocate(symba_plA)
 			USE module_parameters
 			USE module_symba
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)			:: npl
 			TYPE(symba_pl), INTENT(INOUT)		:: symba_plA
 
-			DEALLOCATE(symba_plA%lmerged(npl))
-			DEALLOCATE(symba_plA%nplenc(npl))
-			DEALLOCATE(symba_plA%ntpenc(npl))
-			DEALLOCATE(symba_plA%levelg(npl))
-			DEALLOCATE(symba_plA%levelm(npl))
-			DEALLOCATE(symba_plA%nchild(npl))
-			DEALLOCATE(symba_plA%isperi(npl))
-			DEALLOCATE(symba_plA%peri(npl))
-			DEALLOCATE(symba_plA%atp(npl))
+			DEALLOCATE(symba_plA%lmerged)
+			DEALLOCATE(symba_plA%nplenc)
+			DEALLOCATE(symba_plA%ntpenc)
+			DEALLOCATE(symba_plA%levelg)
+			DEALLOCATE(symba_plA%levelm)
+			DEALLOCATE(symba_plA%nchild)
+			DEALLOCATE(symba_plA%isperi)
+			DEALLOCATE(symba_plA%peri)
+			DEALLOCATE(symba_plA%atp)
 			CALL helio_pl_deallocate(symba_plA%helio,npl)
 			return
 		END SUBROUTINE symba_pl_deallocate
 
-		SUBROUTINE symba_plplenc_deallocate(plplenc_list, nplplenc)
+		SUBROUTINE symba_plplenc_deallocate(plplenc_list)
 			USE module_parameters
 			USE module_symba
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)				:: nplplenc
 			TYPE(symba_plplenc), INTENT(INOUT)		:: plplenc_list
 
-			DEALLOCATE(plplenc_list%lvdotr(nplplenc))
-			DEALLOCATE(plplenc_list%status(nplplenc))
-			DEALLOCATE(plplenc_list%level(nplplenc))
-			DEALLOCATE(plplenc_list%id1(nplplenc))
-			DEALLOCATE(plplenc_list%id2(nplplenc))
+			DEALLOCATE(plplenc_list%lvdotr)
+			DEALLOCATE(plplenc_list%status)
+			DEALLOCATE(plplenc_list%level)
+			DEALLOCATE(plplenc_list%id1)
+			DEALLOCATE(plplenc_list%id2)
 			return
 		END SUBROUTINE symba_plplenc_deallocate
 
-		SUBROUTINE symba_merger_deallocate(mergeadd_list, nmergeadd)
+		SUBROUTINE symba_merger_deallocate(mergeadd_list)
 			USE module_parameters
 			USE module_symba
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)				:: nmergeadd
 			TYPE(symba_merger), INTENT(INOUT)		:: mergeadd_list
 
-			DEALLOCATE(mergeadd_list%id(nmergeadd))
-			DEALLOCATE(mergeadd_list%status(nmergeadd))
-			DEALLOCATE(mergeadd_list%ncomp(nmergeadd))
-			DEALLOCATE(mergeadd_list%xh(NDIM,nmergeadd))
-			DEALLOCATE(mergeadd_list%vh(NDIM,nmergeadd))
-			DEALLOCATE(mergeadd_list%mass(nmergeadd))
-			DEALLOCATE(mergeadd_list%radius(nmergeadd))
+			DEALLOCATE(mergeadd_list%id)
+			DEALLOCATE(mergeadd_list%status)
+			DEALLOCATE(mergeadd_list%ncomp)
+			DEALLOCATE(mergeadd_list%xh)
+			DEALLOCATE(mergeadd_list%vh)
+			DEALLOCATE(mergeadd_list%mass)
+			DEALLOCATE(mergeadd_list%radius)
 			return
 		END SUBROUTINE symba_merger_deallocate
 
-		SUBROUTINE swiftest_tp_deallocate(swiftest_tpA, ntp)
+		SUBROUTINE swiftest_tp_deallocate(swiftest_tpA)
 			USE module_parameters
 			USE module_swiftest
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)			:: ntp
 			TYPE(swiftest_tp), INTENT(INOUT)	:: swiftest_tpA
 
-			DEALLOCATE(swiftest_tpA%id(ntp))
-         	DEALLOCATE(swiftest_tpA%status(ntp))
-         	DEALLOCATE(swiftest_tpA%peri(ntp))
-         	DEALLOCATE(swiftest_tpA%atp(ntp))
-         	DEALLOCATE(swiftest_tpA%isperi(ntp))
-         	DEALLOCATE(swiftest_tpA%xh(NDIM,ntp))
-         	DEALLOCATE(swiftest_tpA%vh(NDIM,ntp))
-         	DEALLOCATE(swiftest_tpA%xb(NDIM,ntp))
-         	DEALLOCATE(swiftest_tpA%vb(NDIM,ntp))
+			DEALLOCATE(swiftest_tpA%id)
+         	DEALLOCATE(swiftest_tpA%status)
+         	DEALLOCATE(swiftest_tpA%peri)
+         	DEALLOCATE(swiftest_tpA%atp)
+         	DEALLOCATE(swiftest_tpA%isperi)
+         	DEALLOCATE(swiftest_tpA%xh)
+         	DEALLOCATE(swiftest_tpA%vh)
+         	DEALLOCATE(swiftest_tpA%xb)
+         	DEALLOCATE(swiftest_tpA%vb)
         	return
         END SUBROUTINE swiftest_tp_deallocate
 
 
-        SUBROUTINE helio_tp_deallocate(helio_tpA, ntp)
+        SUBROUTINE helio_tp_deallocate(helio_tpA)
 			USE module_parameters
 			USE module_helio
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)			:: ntp
 			TYPE(helio_tp), INTENT(INOUT)		:: helio_tpA
 
-			DEALLOCATE(helio_tpA%ah(NDIM,ntp))
-         	DEALLOCATE(helio_tpA%ahi(NDIM,ntp))
-         	CALL swiftest_tp_deallocate(helio_tpA%swiftest,ntp)
+			DEALLOCATE(helio_tpA%ah)
+         	DEALLOCATE(helio_tpA%ahi)
+         	CALL swiftest_tp_deallocate(helio_tpA%swiftest)
         	return
         END SUBROUTINE helio_tp_deallocate
 
 
-        SUBROUTINE symba_tp_deallocate(symba_tpA, ntp)
+        SUBROUTINE symba_tp_deallocate(symba_tpA)
 			USE module_parameters
 			USE module_symba
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)			:: ntp
 			TYPE(symba_tp), INTENT(INOUT)		:: symba_tpA
 
-			DEALLOCATE(symba_tpA%nplenc(ntp))
-			DEALLOCATE(symba_tpA%levelg(ntp))
-			DEALLOCATE(symba_tpA%levelm(ntp))
+			DEALLOCATE(symba_tpA%nplenc)
+			DEALLOCATE(symba_tpA%levelg)
+			DEALLOCATE(symba_tpA%levelm)
 			return
 		END SUBROUTINE symba_tp_deallocate
 
-        SUBROUTINE symba_pltpenc_deallocate(pltpenc_list, npltpenc)
+        SUBROUTINE symba_pltpenc_deallocate(pltpenc_list)
 			USE module_parameters
 			USE module_symba
 			IMPLICIT NONE
 
 			! Arguments
-			INTEGER(I4B), INTENT(IN)				:: npltpenc
 			TYPE(symba_pltpenc), INTENT(INOUT)		:: pltpenc_list
 
-			DEALLOCATE(pltpenc_list%lvdotr(npltpenc))
-			DEALLOCATE(pltpenc_list%status(npltpenc))
-			DEALLOCATE(pltpenc_list%level(npltpenc))
-			DEALLOCATE(pltpenc_list%idpl(npltpenc))
-			DEALLOCATE(pltpenc_list%idtp(npltpenc))
+			DEALLOCATE(pltpenc_list%lvdotr)
+			DEALLOCATE(pltpenc_list%status)
+			DEALLOCATE(pltpenc_list%level)
+			DEALLOCATE(pltpenc_list%idpl)
+			DEALLOCATE(pltpenc_list%idtp)
 			return
 		END SUBROUTINE symba_pltpenc_deallocate
 		
