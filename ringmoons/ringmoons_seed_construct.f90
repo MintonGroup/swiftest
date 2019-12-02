b2d28852e4d530265a0ac46f50258b1a27368be8
David Minton <daminton@purdue.edu>
2019-12-01 21:45:57 -0500

    New seed construction model

--- ringmoons/ringmoons_seed_construct.f90
+++ ringmoons/ringmoons_seed_construct.f90
@@ -106,11 +106,9 @@ subroutine ringmoons_seed_construct(swifter_pl1P,ring,seeds)
       
       ! Make seeds small enough to fit into each bin 
       do i = ring%iRRL,ring%N
-         !if (((i > ring%iRRL).and.(ring%Q(i) < 1.0_DP)).or.((i > ring%iFRL).and.(ring%Gm(i) > ring%Gm_pdisk(i)))) then
-         ! See Tajeddine et al. (2017) section 2.3 
-            R_min = (3.3e5 / DU2CM) *  (ring%nu(i) / (100 * TU2S / DU2CM**2))
-            !if (((ring%r_pdisk(i) > R_min).or.(i > ring%iFRL)).and.(ring%Gm(i) > ring%Gm_pdisk(i))) then 
-            if ((ring%r_pdisk(i) > R_min).and.(ring%Gm(i) > ring%Gm_pdisk(i))) then 
+         ! See Tajeddine et al. (2017) section 2.3. 10% the gap opening size
+         R_min = 0.1_DP * (3.3e5 / DU2CM) *  (ring%nu(i) / (100 * TU2S / DU2CM**2))
+         if ((ring%r_pdisk(i) > R_min).and.(ring%Gm(i) > ring%Gm_pdisk(i))) then 
                open_space = .not.any(seeds%rbin(:) == i .and. seeds%active(:))
                if (open_space) then
                   a = ring%r(i)
