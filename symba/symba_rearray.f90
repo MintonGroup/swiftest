3e05c3f2e8de68cd288159e9243979da620140fc
Jennifer <jpouplin@purdue.edu>
2019-12-04 12:28:24 -0500

    edit typo symba-rearray

--- symba/symba_rearray.f90
+++ symba/symba_rearray.f90
@@ -92,7 +92,6 @@ SUBROUTINE symba_rearray(t, npl, ntp, nsppl, nsptp, symba_plA, symba_tpA, nmerge
         	END IF 
     	END DO
 
-        nsppl = nsppl + npl - nkpl
         npl = nkpl
 
         ! remove all mentions of mergeadd and mergeadd_list from this loop after fragmentation 
@@ -161,7 +160,6 @@ SUBROUTINE symba_rearray(t, npl, ntp, nsppl, nsptp, symba_plA, symba_tpA, nmerge
         	END IF 
     	END DO 
 
-        nsptp = nsptp + ntp - nktp
         ntp = nktp
 
     	CALL symba_tp_deallocate(symba_tpA)
