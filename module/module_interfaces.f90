c0dbfd0361a3dcfbf5819db514322e43ec5c44b0
Carlisle Wishard <cwishard@purdue.edu>
2020-02-17 13:11:15 -0500

    updated module_interfaces for merge_tp

--- module/module_interfaces.f90
+++ module/module_interfaces.f90
@@ -891,7 +891,7 @@ MODULE module_interfaces
                CHARACTER(*), INTENT(IN)                         :: encounter_file, out_type
                TYPE(symba_plplenc), INTENT(INOUT)               :: plplenc_list
                TYPE(symba_merger), INTENT(INOUT)                :: mergeadd_list, mergesub_list
-          END SUBROUTINE symba_fragmentation_pl
+          END SUBROUTINE symba_fragmentation
      END INTERFACE
 
      INTERFACE
@@ -1020,7 +1020,7 @@ MODULE module_interfaces
      END INTERFACE
 
      INTERFACE
-          SUBROUTINE symba_merge_tp(t, dt, index_enc, npltpenc, pltpenc_list, vbs, encounter_file, out_type)
+          SUBROUTINE symba_merge_tp(t, dt, index_enc, npltpenc, pltpenc_list, vbs, encounter_file, out_type, symba_plA, symba_tpA)
                USE module_parameters
                USE module_swifter
                USE module_helio
@@ -1031,6 +1031,8 @@ MODULE module_interfaces
                REAL(DP), DIMENSION(NDIM), INTENT(IN)  :: vbs
                CHARACTER(*), INTENT(IN)               :: encounter_file, out_type
                TYPE(symba_pltpenc), INTENT(INOUT)     :: pltpenc_list
+               TYPE(symba_pl), INTENT(INOUT)                    :: symba_plA
+               TYPE(symba_tp), INTENT(INOUT)                    :: symba_tpA
           END SUBROUTINE symba_merge_tp
      END INTERFACE
 
