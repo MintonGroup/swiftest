a2724f4b9ad0b1d82118c664c71963ca8e49aa08
Jake Elliott <elliot26@purdue.edu>
2020-03-06 14:58:54 -0500

    temporarily set nplm = npl in symba_step

--- symba/symba_step.f90
+++ symba/symba_step.f90
@@ -265,7 +265,7 @@ SUBROUTINE symba_step(lfirst, lextra_force, lclose, t, npl, nplmax, ntp, ntpmax,
 ! END OF THINGS THAT NEED TO BE CHANGED IN THE TREE
 
      ! temp
-     ! nplm = npl
+     nplm = npl
 
      ! flag to see if there was an encounter
      lencounter = ((nplplenc > 0) .OR. (npltpenc > 0))
