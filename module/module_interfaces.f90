780d3eaa7f408dbeb0a7619d79ffd8f5f55ace31
Jake Elliott <elliot26@purdue.edu>
2020-04-07 16:16:44 -0400

    modify module_interfaces for k_plpl in util_dist_index_plpl

--- module/module_interfaces.f90
+++ module/module_interfaces.f90
@@ -1429,14 +1429,14 @@ MODULE module_interfaces
      END INTERFACE
 
      INTERFACE
-          SUBROUTINE util_dist_index_plpl(npl, mass, mtiny, num_comparisons, ik_plpl, jk_plpl)
+          SUBROUTINE util_dist_index_plpl(npl, mass, mtiny, num_comparisons, k_plpl)
                USE module_parameters
                USE module_swiftest
                IMPLICIT NONE
                REAL(DP), DIMENSION(npl), INTENT(IN) :: mass
                INTEGER(I4B), INTENT(IN)  :: npl
                REAL (DP), INTENT(IN) :: mtiny
-               INTEGER(I4B), DIMENSION(:),ALLOCATABLE,INTENT(OUT) :: ik_plpl, jk_plpl
+               INTEGER(I4B), DIMENSION(:,:),ALLOCATABLE,INTENT(OUT) :: k_plpl
                INTEGER(I4B), INTENT(OUT) :: num_comparisons
           END SUBROUTINE
      END INTERFACE
