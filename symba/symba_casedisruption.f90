444b97ad38ecdfb2085fd2f52cddb04d916bf146
Carlisle Wishard <cwishard@purdue.edu>
2020-05-11 16:13:17 -0400

    Implemented Durda et al 2007

--- symba/symba_casedisruption.f90
+++ symba/symba_casedisruption.f90
@@ -64,6 +64,7 @@ SUBROUTINE symba_casedisruption (t, dt, index_enc, nmergeadd, nmergesub, mergead
      INTEGER(I4B)                                     :: model, nres, nfrag
      REAL(DP)                                         :: m1, m2, rad1, rad2, pres, vres, mtot, msun, mtot, mv, avg_d, dp_1, dp_2 
      REAL(DP)                                         :: r, mu, vdot, energy, ap, rhill_p1, rhill_p2, pi, r_circle, theta
+     REAL(DP)                                         :: m_rem, m_test
      REAL(DP)                                         :: x_com, y_com, z_com, vx_com, vy_com, vz_com
      REAL(DP)                                         :: x_frag, y_frag, z_frag, vx_frag, vy_frag, fz_frag
      REAL(DP), DIMENSION(NDIM)                        :: x1, x2, v1, v2, xbs, xh, xb, vb, vh
@@ -184,44 +185,54 @@ SUBROUTINE symba_casedisruption (t, dt, index_enc, nmergeadd, nmergesub, mergead
      mtot = 0.0_DP ! running total mass of new fragments
      mv = 0.0_DP   ! running sum of m*v of new fragments to be used in COM calculation
      DO i = 1, nfrag
-          nmergeadd = nmergeadd + 1
-          mergeadd_list%name(nmergeadd) = nplmax + ntpmax + fragmax + i
-          mergeadd_list%status(nmergeadd) = ACTIVE
-          mergeadd_list%ncomp(nmergeadd) = 2
-          IF (i == 1) THEN
-               ! first largest particle from collresolve mres[0] rres[0]
-               mergeadd_list%mass(nmergeadd) = mres(1)
-               mergeadd_list%radius(nmergeadd) = rres(1)
-               mtot = mtot + mergeadd_list%mass(nmergeadd)                             
-          END IF
-          IF (i == 2) THEN
-               ! second largest particle from collresolve mres[1] rres[1]
-               mergeadd_list%mass(nmergeadd) = mres(2)
-               mergeadd_list%radius(nmergeadd) = rres(2) 
-               mtot = mtot + mergeadd_list%mass(nmergeadd)                            
-          END IF
-          IF (i > 2) THEN
-               ! FIXME all other particles implement eq. 31 LS12
-               d_p1 = (3.0_DP * m1) / (4.0_DP * pi * (rad1 ** 3.0_DP))
-               d_p2 = (3.0_DP * m2) / (4.0_DP * pi * (rad2 ** 3.0_DP))
-               avg_d = (d_p1 + d_p2) / 2.0_DP
-               mergeadd_list%mass(nmergeadd) = (m1 + m2) - (mres(1) + mres(2))
-               mergeadd_list%radius(nmergeadd) = ((3.0_DP * mergeadd_list%mass(nmergeadd)) / (4.0_DP * pi * avg_d))  ** (1.0_DP / 3.0_DP) 
-               mtot = mtot + mergeadd_list%mass(nmergeadd)                                                              
-          END IF                                  
-          x_frag = (r_circle * cos(theta * i)) + x_com
-          y_frag = (r_cirlce * sin(theta * i)) + y_com
-          z_frag = z_com
-          vx_frag = ((1 / nfrag) * (1 / mergeadd_list%mass(nmergeadd)) * ((m1 * v1(1)) + (m2 * v2(1)))) - vbs(1)
-          vy_frag = ((1 / nfrag) * (1 / mergeadd_list%mass(nmergeadd)) * ((m1 * v1(2)) + (m2 * v2(2)))) - vbs(2)
-          vz_frag = vz_com - vbs(3)
-          mergeadd_list%xh(1,nmergeadd) = x_frag
-          mergeadd_list%xh(2,nmergeadd) = y_frag 
-          mergeadd_list%xh(3,nmergeadd) = z_frag                                                    
-          mergeadd_list%vh(1,nmergeadd) = vx_frag
-          mergeadd_list%vh(2,nmergeadd) = vy_frag
-          mergeadd_list%vh(3,nmergeadd) = vz_frag
-          mv = mv + (mergeadd_list%mass(nmergeadd) * mergeadd_list%(:,nmergeadd))
+         nmergeadd = nmergeadd + 1
+         mergeadd_list%name(nmergeadd) = nplmax + ntpmax + fragmax + i
+         mergeadd_list%status(nmergeadd) = ACTIVE
+         mergeadd_list%ncomp(nmergeadd) = 2
+         IF (i == 1) THEN
+             ! first largest particle from collresolve mres[0] rres[0]
+             mergeadd_list%mass(nmergeadd) = mres(1)
+             mergeadd_list%radius(nmergeadd) = rres(1)
+             mtot = mtot + mergeadd_list%mass(nmergeadd)                             
+         END IF
+         IF (i == 2) THEN
+             ! second largest particle from collresolve mres[1] rres[1]
+             mergeadd_list%mass(nmergeadd) = mres(2)
+             mergeadd_list%radius(nmergeadd) = rres(2) 
+             mtot = mtot + mergeadd_list%mass(nmergeadd)                            
+         END IF
+         IF (i > 2) THEN
+             ! FIXME all other particles implement eq. 31 LS12
+             ! FIXME current equation taken from Durda et al 2007 Figure 2 Supercatastrophic: N = (1.5e5)e(-1.3*D)
+             d_p1 = (3.0_DP * m1) / (4.0_DP * pi * (rad1 ** 3.0_DP))
+             d_p2 = (3.0_DP * m2) / (4.0_DP * pi * (rad2 ** 3.0_DP))
+             avg_d = (d_p1 + d_p2) / 2.0_DP
+
+             m_rem = (m1 + m2) - (mres(1) + mres(2))
+             m_test = (((- 1.0_DP / 2.6_DP) * log(i / (1.5_DP * 10.0_DP ** 5))) ** 3.0_DP) * ((4.0_DP / 3.0_DP) * pi * avg_d)
+             
+             IF (m_test < m_rem):
+             	 mergeadd_list%mass(nmergeadd) = m_test
+             ELSE
+             	 mergeadd_list%mass(nmergeadd) = (m1 + m2) - mtot 
+             END IF 
+             mergeadd_list%radius(nmergeadd) = ((3.0_DP * mergeadd_list%mass(nmergeadd)) / (4.0_DP * pi * avg_d))  & 
+             	 ** (1.0_DP / 3.0_DP) 
+             mtot = mtot + mergeadd_list%mass(nmergeadd)                                                              
+         END IF                                  
+         x_frag = (r_circle * cos(theta * i)) + x_com
+         y_frag = (r_cirlce * sin(theta * i)) + y_com
+         z_frag = z_com
+         vx_frag = ((1 / nfrag) * (1 / mergeadd_list%mass(nmergeadd)) * ((m1 * v1(1)) + (m2 * v2(1)))) - vbs(1)
+         vy_frag = ((1 / nfrag) * (1 / mergeadd_list%mass(nmergeadd)) * ((m1 * v1(2)) + (m2 * v2(2)))) - vbs(2)
+         vz_frag = vz_com - vbs(3)
+         mergeadd_list%xh(1,nmergeadd) = x_frag
+         mergeadd_list%xh(2,nmergeadd) = y_frag 
+         mergeadd_list%xh(3,nmergeadd) = z_frag                                                    
+         mergeadd_list%vh(1,nmergeadd) = vx_frag
+         mergeadd_list%vh(2,nmergeadd) = vy_frag
+         mergeadd_list%vh(3,nmergeadd) = vz_frag
+         mv = mv + (mergeadd_list%mass(nmergeadd) * mergeadd_list%(:,nmergeadd))
      END DO
 
      ! Calculate energy after frag                                                                           
