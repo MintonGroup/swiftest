c3dd09139b1392874499f3c5500367e4a10cc7e3
Carlisle Wishard <cwishard@purdue.edu>
2020-05-12 14:06:15 -0400

    module interfaces update remove swiftest_pla

--- module/module_interfaces.f90
+++ module/module_interfaces.f90
@@ -1029,7 +1029,7 @@ MODULE module_interfaces
           SUBROUTINE symba_fragmentation(t, dt, index_enc, nmergeadd, nmergesub, mergeadd_list, &
                mergesub_list, eoffset, vbs, encounter_file, out_type, npl, ntp, &
                symba_plA, symba_tpA, nplplenc, npltpenc, pltpenc_list, plplenc_list, &
-               swiftest_plA, swiftest_tpA, nplmax, ntpmax, fragmax)
+               nplmax, ntpmax, fragmax)
                USE module_parameters
                USE module_swiftest
                USE module_helio
@@ -1047,8 +1047,6 @@ MODULE module_interfaces
                TYPE(symba_merger), INTENT(INOUT)                :: mergeadd_list, mergesub_list
                TYPE(symba_pl), INTENT(INOUT)                    :: symba_plA
                TYPE(symba_tp), INTENT(INOUT)                    :: symba_tpA
-               TYPE(swiftest_pl), INTENT(INOUT)                 :: swiftest_plA
-               TYPE(swiftest_tp), INTENT(INOUT)                 :: swiftest_tpA
           END SUBROUTINE symba_fragmentation
      END INTERFACE
 
