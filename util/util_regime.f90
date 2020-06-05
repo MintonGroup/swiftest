6f40fa99dd53f7907f2793b42eb81b0301241a12
Carlisle Wishard <cwishard@purdue.edu>
2020-06-05 14:00:07 -0400

    debuged util_regime

--- util/util_regime.f90
+++ util/util_regime.f90
@@ -33,7 +33,7 @@ SUBROUTINE util_regime(symba_plA, index1, index2)
      USE module_helio
      USE module_nrutil
      USE module_swiftestalloc
-     USE module_interfaces, EXCEPT_THIS_ONE => util_resize_pl
+     USE module_interfaces, EXCEPT_THIS_ONE => util_regime
      IMPLICIT NONE
 
 ! Arguments
@@ -41,11 +41,10 @@ SUBROUTINE util_regime(symba_plA, index1, index2)
      INTEGER(I4B), INTENT(IN)      :: index1, index2
 
 ! Internals
-     TYPE(symba_pl)                :: 
-     REAL(DP)                      :: GU, m1,m2,rad1,rad2,b,l,mu,Vescp,V_pstar, Rp, mtot
+     REAL(DP)                      :: m1,m2,rad1,rad2,b,l,mu,Vescp,V_pstar, Rp, mtot
      REAL(DP)                      :: alpha, QRD_pstar, QR, QR_supercat, QRD_lr, V_lr, vimp, bcrit
-     REAL(DP)                      :: Vcr, V_supercat, Vescp, Mlr, Mint, Lint, Aint, fgamma, theta
-     REAL(DP)                      :: c1, c2,c3,c4,c5, rho1, rho2 
+     REAL(DP)                      :: Vcr, V_supercat, Mlr, Mint, Lint, Aint, fgamma, theta, rtarg, n2g, n2, phi
+     REAL(DP)                      :: c1, c2,c3,c4,c5, rho1, rho2, beta, c_star, density1, g, mp, mtarg, mu_bar, n1, n1g
      REAL(DP), DIMENSION(NDIM)     :: xh1,xh2,vh1,vh2
 ! Constants
      density1 = 1000 ![kg/m3]
@@ -61,19 +60,18 @@ SUBROUTINE util_regime(symba_plA, index1, index2)
      c2 = -0.0408
      c3 = 1.86
      c4 = 1.08
-     c5 = 5/2
+     c5 = 5.0_DP/2.0_DP
 ! Executable code
-     GU = GC * 
-     m1 = symba_pl%helio%swiftest%mass(index1)/GU
-     m2 = symba_pl%helio%swiftest%mass(index2)/GU
-     xh1(:) = symba_pl%helio%swiftest%xh(:,index1)
-     xh2(:) = symba_pl%helio%swiftest%xh(:,index2)
-     vh1(:) = symba_pl%helio%swiftest%vh(:,index1)
-     vh2(:) = symba_pl%helio%swiftest%vh(:,index2)
+     m1 = symba_plA%helio%swiftest%mass(index1)/GC
+     m2 = symba_plA%helio%swiftest%mass(index2)/GC
+     xh1(:) = symba_plA%helio%swiftest%xh(:,index1)
+     xh2(:) = symba_plA%helio%swiftest%xh(:,index2)
+     vh1(:) = symba_plA%helio%swiftest%vh(:,index1)
+     vh2(:) = symba_plA%helio%swiftest%vh(:,index2)
      vimp = NORM2(vh2(:) - vh1(:))
-     rad1 = symba_pl%helio%swiftest%radius(index1)
-     rad2 = symba_pl%helio%swiftest%radius(index2)
-     b = 
+     rad1 = symba_plA%helio%swiftest%radius(index1)
+     rad2 = symba_plA%helio%swiftest%radius(index2)
+     b = 1
      l = (rad1 + rad2)*(1-b)
      IF (l < 2*rad2) THEN
           alpha = (l**2.0_DP)*(3*rad2-l)/(4*(rad2**3.0_DP))
@@ -84,18 +82,18 @@ SUBROUTINE util_regime(symba_plA, index1, index2)
      mtot = m1 + m2 
      Rp = (3*(m1+alpha*m2)/(4.0_DP * PI * rho1)**(1.0_DP/3.0_DP))
      
-     Vescp = SQRT(2*GU*mtot/(rad1+rad2))
-     QRD_pstar = 
-     V_pstar = 
+     Vescp = SQRT(2*GC*mtot/(rad1+rad2))
+     QRD_pstar = 1
+     V_pstar = 1
      mu = (m1*m2)/mtot
      QR = 0.5*mu*(vimp**2.0_DP)/mtot
      Mlr = (1.0_DP - 0.5 * QR / QRD_pstar) * (mtot)  ! [kg] #(Eq 5)
-     Phi = 2.o_DP * ACOS((l - Rp) / Rp)
+     Phi = 2.0_DP * ACOS((l - Rp) / Rp)
      Aint = (Rp ** 2.0_DP) * (PI - (Phi - sin(Phi)) / 2.0_DP)
      Lint = 2.0_DP * (Rtarg ** 2.0_DP - (Rtarg - l / 2.0_DP) ** 2.0_DP) ** (1.0_DP/2.0_DP)
-     Mint = Aint * Lint  # [kg]
-     V_lr = calc_erosion(Mtarg, Mp, alpha)
-     QRD_lr, V_lr = calc_QRD_lr(Mp, Mtarg, Mint)
+     Mint = Aint * Lint  ![kg]
+     V_lr = 1.0_DP !calc_erosion(Mtarg, Mp, alpha)
+     QRD_lr = 1.0_DP !calc_QRD_lr(Mp, Mtarg, Mint)
      QR_supercat = 1.8_DP * QRD_pstar
      V_supercat = ((QR_supercat * mtot)/ (0.5 * mu)) ** (1.0_DP / 2.0_DP)
      fgamma = (m1 - m2) / mtot
@@ -103,39 +101,39 @@ SUBROUTINE util_regime(symba_plA, index1, index2)
      Vcr = Vescp * (c1 * fgamma * theta ** c5 + c2 * fgamma + c3 * theta ** c5 + c4)
      bcrit = rad1/(rad1+rad2)
 
-     IF( Vi < Vescp) THEN
+     IF( Vimp < Vescp) THEN
           WRITE(*,*) "regime perfect merging regime"
      ELSE IF (b < bcrit) THEN
           WRITE(*,*) "regime non grazing"
-          IF (Vi < V_lr) THEN
+          IF (Vimp < V_lr) THEN
                WRITE(*,*) "regime partial accretion regime"
-          ELSE IF (vimp > V_lr .AND. Vi < V_supercat) THEN
+          ELSE IF (Vimp > V_lr .AND. Vimp < V_supercat) THEN
                IF (m2 < 1e-3 * m1) THEN
                     WRITE(*,*) "regime cratering"
                ELSE 
                     WRITE(*,*) "regime disruption"
                END IF
-          ELSE IF (Vi > V_supercat) THEN 
+          ELSE IF (Vimp > V_supercat) THEN 
                WRITE(*,*) "regime supercatastrophic"
           ELSE 
                WRITE(*,*) "error"
           END IF
     ELSE IF  (b > bcrit) THEN
           WRITE(*,*) "regime grazing"
-          IF (vimp < V_lr) THEN
-               IF (Vi < Vcr) THEN
+          IF (Vimp < V_lr) THEN
+               IF (Vimp < Vcr) THEN
                     WRITE(*,*) "regime graze and merge"
                ELSE
                     WRITE(*,*) "regime hit and run"
                END IF
                
-          ELSE IF (vimp > V_lr .AND. Vi < V_supercat) THEN
+          ELSE IF (Vimp > V_lr .AND. Vimp < V_supercat) THEN
                IF (m2 < 1e-3 * m1) THEN 
                     WRITE(*,*) "regime cratering"
                ELSE
                     WRITE(*,*) "regime disruption"
                END IF 
-          ELSE IF (vimp > V_supercat) THEN 
+          ELSE IF (Vimp > V_supercat) THEN 
                WRITE(*,*) "regime supercatastrophic"
           END IF 
      END IF 
