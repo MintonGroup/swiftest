00f0546ffec41fa6001a9c383b762f1212949bec
David Minton <daminton@purdue.edu>
2020-06-06 08:32:01 -0400

    Updated new io module with interfaces

--- src/io/io.f90
+++ src/io/io.f90
@@ -1,14 +1,22 @@
 module io
    !! Module containing all input/output subroutine interface blocks 
    use module_parameters
-   implicit none
+   use module_interfaces
 
    interface
       module function io_read_param_in(inparfile) result(param)
-         implicit none
          type(input_parameters) :: param
          character(*), intent(in)  :: inparfile
       end function io_read_param_in
+
+      module function io_get_token(buffer, ilength, ifirst, ilast, ierr) result(token)
+         integer(I4B), intent(in)     :: ilength
+         integer(I4B), intent(inout)  :: ifirst
+         integer(I4B), intent(out)    :: ilast, ierr
+         character(len=*), intent(in) :: buffer
+         character(len=len(buffer))   :: token
+      end function io_get_token
+         
    end interface
 
 end module io
