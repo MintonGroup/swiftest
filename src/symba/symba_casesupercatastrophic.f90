0c13bb049725c9fa4db9cec5f76fdf30251ed74d
Carlisle Wishard <cwishard@purdue.edu>
2020-06-10 16:33:18 -0400

    added msun and removed STOP in casesupercatastrophic

--- src/symba/symba_casesupercatastrophic.f90
+++ src/symba/symba_casesupercatastrophic.f90
@@ -150,12 +150,11 @@ SUBROUTINE symba_casesupercatastrophic (t, dt, index_enc, nmergeadd, nmergesub, 
      rhill_p2 = symba_plA%helio%swiftest%rhill(index2_parent)
      r_circle = (rhill_p1 + rhill_p2) / (2.0_DP * sin(PI / nfrag))
      theta = (2.0_DP * PI) / nfrag
+     msun = symba_plA%helio%swiftest%mass(1)
      semimajor_inward = ((dt * 32.0_DP) ** 2.0_DP) ** (1.0_DP / 3.0_DP)
      CALL orbel_xv2aeq(x1, v1, msun, semimajor_encounter, e, q)
-
     IF (semimajor_inward > (semimajor_encounter - r_circle)) THEN
         WRITE(*,*) "Timestep is too large to resolve fragments."
-        STOP
     ELSE
 
         ! Add new fragments to mergeadd_list
