e2a689b3e42d4de716f3b0aa6cd421dd27415f63
Carlisle Wishard <cwishard@purdue.edu>
2020-06-10 12:03:19 -0400

    restructured hitandrun IF for case of perfect hit and run

--- src/symba/symba_casehitandrun.f90
+++ src/symba/symba_casehitandrun.f90
@@ -170,27 +170,7 @@ SUBROUTINE symba_casehitandrun (t, index_enc, nmergeadd, nmergesub, mergeadd_lis
              mergeadd_list%name(nmergeadd) = nplmax + ntpmax + fragmax + i - 1
              mergeadd_list%mass(nmergeadd) = mres(2)
              mergeadd_list%radius(nmergeadd) = rres(2) 
-             mtot = mtot + mergeadd_list%mass(nmergeadd)       
-         END IF
-         IF (i > 2) THEN
-             ! FIXME all other particles implement eq. 31 LS12
-             ! FIXME current equation taken from Durda et al 2007 Figure 2 Supercatastrophic: N = (1.5e5)e(-1.3*D)
-             mergeadd_list%name(nmergeadd) = nplmax + ntpmax + fragmax + i - 1
-             m_rem = m_rm - mres(2)
-             m_test = (((- 1.0_DP / 2.6_DP) * log(i / (1.5_DP * 10.0_DP ** 5))) ** 3.0_DP) * ((4.0_DP / 3.0_DP) * PI * d_rm)
-             IF (m_test < m_rem) THEN
-                mergeadd_list%mass(nmergeadd) = m_test
-             ELSE
-                mergeadd_list%mass(nmergeadd) = (m1 + m2) - mtot
-                WRITE(*, *) "HIT AND RUN mtot : ", mtot
-             END IF 
-             WRITE(*, *) "HIT AND RUN CHECK 1"
-             mergeadd_list%radius(nmergeadd) = ((3.0_DP * mergeadd_list%mass(nmergeadd)) / (4.0_DP * PI * d_rm))  & 
-                ** (1.0_DP / 3.0_DP) 
-             mtot = mtot + mergeadd_list%mass(nmergeadd)
-         END IF
-         WRITE(*, *) "HIT AND RUN CHECK 2"
-         IF (i > 1) THEN
+             mtot = mtot + mergeadd_list%mass(nmergeadd) 
              x_frag = (r_circle * cos(theta * i)) + x_com
              y_frag = (r_circle * sin(theta * i)) + y_com
              z_frag = z_com
@@ -202,18 +182,44 @@ SUBROUTINE symba_casehitandrun (t, index_enc, nmergeadd, nmergesub, mergeadd_lis
              mergeadd_list%xh(3,nmergeadd) = z_frag                                                    
              mergeadd_list%vh(1,nmergeadd) = vx_frag
              mergeadd_list%vh(2,nmergeadd) = vy_frag
-             mergeadd_list%vh(3,nmergeadd) = vz_frag   
-          END IF                                             
+             mergeadd_list%vh(3,nmergeadd) = vz_frag        
+         END IF
+         IF (i > 2) THEN
+            m_rem = m_rm - mres(2)
+            IF (m_rem > (m_rm) / 1000.0_DP) THEN
+             ! FIXME all other particles implement eq. 31 LS12
+             ! FIXME current equation taken from Durda et al 2007 Figure 2 Supercatastrophic: N = (1.5e5)e(-1.3*D)
+                mergeadd_list%name(nmergeadd) = nplmax + ntpmax + fragmax + i - 1
+                m_test = (((- 1.0_DP / 2.6_DP) * log(i / (1.5_DP * 10.0_DP ** 5.0_DP))) ** 3.0_DP) * ((4.0_DP / 3.0_DP) * PI * d_rm)
+                IF (m_test < m_rem) THEN
+                    mergeadd_list%mass(nmergeadd) = m_test  
+                ELSE
+                    mergeadd_list%mass(nmergeadd) = (m1 + m2) - mtot
+                END IF 
+                mergeadd_list%radius(nmergeadd) = ((3.0_DP * mergeadd_list%mass(nmergeadd)) / (4.0_DP * PI * d_rm))  & 
+                    ** (1.0_DP / 3.0_DP) 
+                mtot = mtot + mergeadd_list%mass(nmergeadd)
+                x_frag = (r_circle * cos(theta * i)) + x_com
+                y_frag = (r_circle * sin(theta * i)) + y_com
+                z_frag = z_com
+                vx_frag = ((1.0_DP / (nfrag-1)) * (1.0_DP / mergeadd_list%mass(nmergeadd)) * ((m_rm * vx_rm))) - vbs(1)
+                vy_frag = ((1.0_DP / (nfrag-1)) * (1.0_DP / mergeadd_list%mass(nmergeadd)) * ((m_rm * vy_rm))) - vbs(2)
+                vz_frag = vz_com - vbs(3)
+                mergeadd_list%xh(1,nmergeadd) = x_frag
+                mergeadd_list%xh(2,nmergeadd) = y_frag 
+                mergeadd_list%xh(3,nmergeadd) = z_frag                                                    
+                mergeadd_list%vh(1,nmergeadd) = vx_frag
+                mergeadd_list%vh(2,nmergeadd) = vy_frag
+                mergeadd_list%vh(3,nmergeadd) = vz_frag 
+            END IF  
+         END IF                                           
           mv = mv + (mergeadd_list%mass(nmergeadd) * mergeadd_list%vh(:,nmergeadd))
      END DO
-     WRITE(*, *) "HIT AND RUN CHECK 3"
      ! Calculate energy after frag                                                                           
      vnew(:) = mv / mtot    ! COM of new fragments                               
      enew = 0.5_DP*mtot*DOT_PRODUCT(vnew(:), vnew(:))
      eoffset = eoffset + eold - enew
-     WRITE(*, *) "HIT AND RUN CHECK 4"
      ! Update fragmax to account for new fragments
      fragmax = fragmax + (nfrag - 1)
-     WRITE(*,*) "Leaving Hit and Run"
      RETURN 
 END SUBROUTINE symba_casehitandrun
