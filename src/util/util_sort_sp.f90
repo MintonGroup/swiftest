df3b4b9a4c358a5c5d033427681abe0843f1a73c
David Minton <daminton@purdue.edu>
2020-06-16 16:56:30 -0400

    Updated one last util subroutine

--- src/util/util_sort_sp.f90
+++ src/util/util_sort_sp.f90
@@ -1,128 +1,86 @@
-!**********************************************************************************************************************************
-!
-!  Unit Name   : util_sort_sp
-!  Unit Type   : subroutine
-!  Project     : Swifter
-!  Package     : util
-!  Language    : Fortran 90/95
-!
-!  Description : Sort input single precision array into ascending numerical order using Quicksort algorithm
-!
-!  Input
-!    Arguments : arr : array to sort
-!    Terminal  : none
-!    File      : none
-!
-!  Output
-!    Arguments : arr : sorted array
-!    Terminal  : error message
-!    File      : none
-!
-!  Invocation  : CALL util_sort(arr)
-!
-!  Notes       : Adapted from Numerical Recipes in Fortran 90: The Art of Parallel Scientific Computing, by Press, Teukolsky,
-!                Vetterling, and Flannery, 2nd ed., pp. 1169-70
-!
-!**********************************************************************************************************************************
-SUBROUTINE util_sort_sp(arr)
+submodule (util) s_util_sort_sp
+contains
+   module procedure util_sort_sp
+   !! author: David A. Minton
+   !!
+   !! Sort input single precision array into ascending numerical order using Quicksort algorithm
+   !!
+   !! Adapted from David E. Kaufmann's Swifter modules: util_sort_DP.f90
+   !! Adapted from Numerical Recipes in Fortran 90: The Art of Parallel Scientific Computing, by Press, Teukolsky,
+   !!      Vetterling, and Flannery, 2nd ed., pp. 1169-7
+   use swiftest
+   integer(I4B), parameter       :: NN = 15, NSTACK = 50
+   real(sp)                :: a, dum
+   integer(I4B)            :: n, k, i, j, jstack, l, r
+   integer(I4B), dimension(NSTACK) :: istack
 
-! Modules
-     use swiftest, EXCEPT_THIS_ONE => util_sort_sp
-     IMPLICIT NONE
+! executable code
+   n = size(arr)
+   jstack = 0
+   l = 1
+   r = n
+   do
+      if ((r - l) < NN) then
+         do j = l + 1, r
+            a = arr(j)
+            do i = j - 1, l, -1
+               if (arr(i) <= a) exit
+               arr(i+1) = arr(i)
+            end do
+            arr(i+1) = a
+         end do
+         if (jstack == 0) return
+         r = istack(jstack)
+         l = istack(jstack-1)
+         jstack = jstack - 2
+      else
+         k = (l + r)/2
+         dum = arr(k); arr(k) = arr(l+1); arr(l+1) = dum
+         if (arr(l) > arr(r)) then
+            dum = arr(l); arr(l) = arr(r); arr(r) = dum
+         end if
+         if (arr(l+1) > arr(r)) then
+            dum = arr(l+1); arr(l+1) = arr(r); arr(r) = dum
+         end if
+         if (arr(l) > arr(l+1)) then
+            dum = arr(l); arr(l) = arr(l+1); arr(l+1) = dum
+         end if
+         i = l + 1
+         j = r
+         a = arr(l+1)
+         do
+            do
+               i = i + 1
+               if (arr(i) >= a) exit
+            end do
+            do
+               j = j - 1
+               if (arr(j) <= a) exit
+            end do
+            if (j < i) exit
+            dum = arr(i); arr(i) = arr(j); arr(j) = dum
+         end do
+         arr(l+1) = arr(j)
+         arr(j) = a
+         jstack = jstack + 2
+         if (jstack > NSTACK) then
+            write(*, *) "swifter error:"
+            write(*, *) "   NSTACK too small in util_sort_I4B"
+            call util_exit(failure)
+         end if
+         if ((r - i + 1) >= (j - l)) then
+            istack(jstack) = r
+            istack(jstack-1) = i
+            r = j - 1
+         else
+            istack(jstack) = j - 1
+            istack(jstack-1) = l
+            l = i
+         end if
+      end if
+   end do
 
-! Arguments
-     REAL(SP), DIMENSION(:), INTENT(INOUT) :: arr
+   return
 
-! Internals
-     INTEGER(I4B), PARAMETER         :: NN = 15, NSTACK = 50
-     REAL(SP)                        :: a, dum
-     INTEGER(I4B)                    :: n, k, i, j, jstack, l, r
-     INTEGER(I4B), DIMENSION(NSTACK) :: istack
-
-! Executable code
-     n = SIZE(arr)
-     jstack = 0
-     l = 1
-     r = n
-     DO
-          IF ((r - l) < NN) THEN
-               DO j = l + 1, r
-                    a = arr(j)
-                    DO i = j - 1, l, -1
-                         IF (arr(i) <= a) EXIT
-                         arr(i+1) = arr(i)
-                    END DO
-                    arr(i+1) = a
-               END DO
-               IF (jstack == 0) RETURN
-               r = istack(jstack)
-               l = istack(jstack-1)
-               jstack = jstack - 2
-          ELSE
-               k = (l + r)/2
-               dum = arr(k); arr(k) = arr(l+1); arr(l+1) = dum
-               IF (arr(l) > arr(r)) THEN
-                    dum = arr(l); arr(l) = arr(r); arr(r) = dum
-               END IF
-               IF (arr(l+1) > arr(r)) THEN
-                    dum = arr(l+1); arr(l+1) = arr(r); arr(r) = dum
-               END IF
-               IF (arr(l) > arr(l+1)) THEN
-                    dum = arr(l); arr(l) = arr(l+1); arr(l+1) = dum
-               END IF
-               i = l + 1
-               j = r
-               a = arr(l+1)
-               DO
-                    DO
-                         i = i + 1
-                         IF (arr(i) >= a) EXIT
-                    END DO
-                    DO
-                         j = j - 1
-                         IF (arr(j) <= a) EXIT
-                    END DO
-                    IF (j < i) EXIT
-                    dum = arr(i); arr(i) = arr(j); arr(j) = dum
-               END DO
-               arr(l+1) = arr(j)
-               arr(j) = a
-               jstack = jstack + 2
-               IF (jstack > NSTACK) THEN
-                    WRITE(*, *) "SWIFTER Error:"
-                    WRITE(*, *) "   NSTACK too small in util_sort_i4b"
-                    CALL util_exit(FAILURE)
-               END IF
-               IF ((r - i + 1) >= (j - l)) THEN
-                    istack(jstack) = r
-                    istack(jstack-1) = i
-                    r = j - 1
-               ELSE
-                    istack(jstack) = j - 1
-                    istack(jstack-1) = l
-                    l = i
-               END IF
-          END IF
-     END DO
-
-     RETURN
-
-END SUBROUTINE util_sort_sp
-!**********************************************************************************************************************************
-!
-!  Author(s)   : David E. Kaufmann
-!
-!  Revision Control System (RCS) Information
-!
-!  Source File : $RCSfile$
-!  Full Path   : $Source$
-!  Revision    : $Revision$
-!  Date        : $Date$
-!  Programmer  : $Author$
-!  Locked By   : $Locker$
-!  State       : $State$
-!
-!  Modification History:
-!
-!  $Log$
-!**********************************************************************************************************************************
+   end procedure util_sort_sp
+end submodule s_util_sort_sp
