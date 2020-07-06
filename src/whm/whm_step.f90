7ed3e87bc2a23157bb39eac26da20a61beba689a
David Minton <daminton@purdue.edu>
2020-07-06 16:44:45 -0400

    Added a new debugging association

--- src/whm/whm_step.f90
+++ src/whm/whm_step.f90
@@ -29,7 +29,7 @@ contains
                associate(xh => pl%xh, vh => pl%vh, xj => pl%xj, vj => pl%vj) ! These associations aid in debugging with gdb
                   select type(tp => self%tp)
                   class is (whm_tp)
-                  associate(xht => tp%xh, vht => tp%vh) ! These associations aid in debugging with gdb
+                  associate(xht => tp%xh, vht => tp%vh, aht => tp%ah) ! These associations aid in debugging with gdb
                      if (is_pl) then
                         if (.not.allocated(xbeg)) allocate(xbeg(npl,NDIM))
                         xbeg(:, :) = pl%xh(1:npl, :)
