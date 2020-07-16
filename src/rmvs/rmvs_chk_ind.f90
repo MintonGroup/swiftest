e5a3a2268c859235763032ea2f35e343521bf844
David Minton <daminton@purdue.edu>
2020-07-16 08:27:13 -0400

    Created template RMVS class from the WHM class

--- src/rmvs/rmvs_chk_ind.f90
+++ src/rmvs/rmvs_chk_ind.f90
@@ -7,11 +7,10 @@ contains
    !!
    !! Adapted from David E. Kaufmann's Swifter routine: rmvs_chk_ind.f90
    !! Adapted from Hal Levison's Swift routine rmvs_chk_ind.f
-use swiftest
-implicit none
+   use swiftest
+   implicit none
    real(DP) :: r2, v2, vdotr, tmin, r2min
 
-! executable code
    iflag = 0
    r2 = dot_product(xr(:), xr(:))
    if (r2 < r2crit) then
@@ -20,11 +19,11 @@ implicit none
       vdotr = dot_product(vr(:), xr(:))
       if (vdotr < 0.0_DP) then
          v2 = dot_product(vr(:), vr(:))
-         tmin = -vdotr/v2
+         tmin = -vdotr / v2
          if (tmin < dt) then
-            r2min = r2 - vdotr*vdotr/v2
+            r2min = r2 - vdotr**2 / v2
          else
-            r2min = r2 + 2.0_DP*vdotr*dt + v2*dt*dt
+            r2min = r2 + 2 * vdotr * dt + v2 * dt**2
          end if
          r2min = min(r2min, r2)
          if (r2min <= r2crit) iflag = 1
