0d81e0d25124bc8b1b757eedc1d1d2292fea332d
Carlisle Wishard <cwishard@purdue.edu>
2021-03-26 16:08:27 -0400

    whm_user_getacch.f90 procedure -> subroutine

--- src/whm/whm_user_getacch.f90
+++ src/whm/whm_user_getacch.f90
@@ -1,6 +1,6 @@
 submodule(whm_classes) s_whm_user_getacch
 contains
-   module procedure whm_user_getacch_pl
+   module subroutine whm_user_getacch_pl(self, cb, config, t)
    !! author: David A. Minton
    !!
    !! Add user-supplied heliocentric accelerations to planets
@@ -9,25 +9,31 @@ contains
    !! Adapted from David E. Kaufmann's Swifter routine whm_user_getacch.f90
    use swiftest
    implicit none
-
+   !! Arguments
+   class(whm_pl),                 intent(inout) :: self   !! WHM massive body particle data structure
+   class(whm_cb),                 intent(inout) :: cb     !! WHM central body particle data structuree
+   class(swiftest_configuration), intent(in)    :: config !! Input collection of 
+   real(DP),                      intent(in)    :: t      !! Current time
 
    return
+   end subroutine whm_user_getacch_pl
 
-   end procedure whm_user_getacch_pl
-
-   module procedure whm_user_getacch_tp
-      !! author: David A. Minton
-      !!
-      !! Add user-supplied heliocentric accelerations to test particles
-      !!
-      !! 
-      !! Adapted from David E. Kaufmann's Swifter routine whm_user_getacch_tp.f90
-      use swiftest
-      implicit none
+   module subroutine whm_user_getacch_tp(self, cb, config, t)
+   !! author: David A. Minton
+   !!
+   !! Add user-supplied heliocentric accelerations to test particles
+   !!
+   !! 
+   !! Adapted from David E. Kaufmann's Swifter routine whm_user_getacch_tp.f90
+   use swiftest
+   implicit none
+   !! Arguments
+   class(whm_tp),                 intent(inout) :: self   !! WHM test particle data structure
+   class(whm_cb),                 intent(inout) :: cb     !! WHM central body particle data structuree
+   class(swiftest_configuration), intent(in)    :: config    !! Input collection of 
+   real(DP),                      intent(in)    :: t         !! Current time
    
-   ! executable code
+   return
+   end subroutine whm_user_getacch_tp
    
-      return
-   
-      end procedure whm_user_getacch_tp
 end submodule s_whm_user_getacch
