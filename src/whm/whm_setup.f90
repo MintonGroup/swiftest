7694a02ae84774aedcc098ba04e10d170c249b47
David A Minton <daminton@purdue.edu>
2021-07-28 14:16:02 -0400

    Cleaned up WHM initial conditions generator script

--- src/whm/whm_setup.f90
+++ src/whm/whm_setup.f90
@@ -85,6 +85,7 @@ contains
       ! First we need to make sure that the massive bodies are sorted by heliocentric distance before computing jacobies
       call util_set_ir3h(self%pl)
       call self%pl%sort("ir3h", ascending=.false.)
+
       ! Make sure that the discard list gets allocated initially
       call self%tp_discards%setup(self%tp%nbody)
       call self%pl%set_mu(self%cb)
