b9d815ee484d635eb1584489857083e61953408f
David A Minton <daminton@purdue.edu>
2021-08-16 19:14:40 -0400

    Fixed energy term in the merger case

--- src/symba/symba_collision.f90
+++ src/symba/symba_collision.f90
@@ -277,7 +277,7 @@ contains
          pe = 0.0_DP
          do j = 1, nfamily
             do i = j + 1, nfamily
-               pe = pe - pl%mass(i) * pl%mass(j) / norm2(pl%xb(:, i) - pl%xb(:, j))
+               pe = pe - pl%Gmass(i) * pl%mass(j) / norm2(pl%xb(:, i) - pl%xb(:, j))
             end do
          end do
          system%Ecollisions  = system%Ecollisions + pe 
