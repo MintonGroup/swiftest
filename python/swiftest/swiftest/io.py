5e5ebd183dea61bfdc32aeadabd612e8e700e334
David A Minton <daminton@purdue.edu>
2021-09-15 15:48:42 -0400

    Added more verbose text feedback on reading NetCDF file

--- python/swiftest/swiftest/io.py
+++ python/swiftest/swiftest/io.py
@@ -694,7 +694,7 @@ def swiftest2xr(param):
         ds = xr.combine_by_coords([cbds, plds, tpds])
 
     elif ((param['OUT_TYPE'] == 'NETCDF_DOUBLE') or (param['OUT_TYPE'] == 'NETCDF_FLOAT')):
-        print('\nCreating Dataset')
+        print('\nCreating Dataset from NetCDF file')
         ds = xr.open_dataset(param['BIN_OUT'], mask_and_scale=False)
         ds = clean_string_values(param, ds)
     else:
