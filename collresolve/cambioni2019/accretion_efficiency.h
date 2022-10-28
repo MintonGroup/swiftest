791895fec9d2de7a335a15d7d47295de9bdde976
Carlisle Wishard <cwishard@purdue.edu>
2022-10-28 17:24:19 -0400

    deleted collresolve directory

--- collresolve/cambioni2019/accretion_efficiency.h
+++
@@ -1,6 +0,0 @@
-#ifndef ACCRETION_EFFICIENCY_H
-#define ACCRETION_EFFICIENCY_H
-
-void accretion_efficiency( const double X[ 4 ], double Y[ 2 ] );
-
-#endif
--- collresolve/cambioni2019/orbital_hnr.h
+++
@@ -1,6 +0,0 @@
-#ifndef ORBITAL_HNR_H
-#define ORBITAL_HNR_H
-
-void orbital_hnr( const double X[ 4 ], double Y[ 3 ] );
-
-#endif
