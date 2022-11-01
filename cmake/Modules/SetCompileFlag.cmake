0e7bd3191c1e023419be9e69aed2b963e684629f
Carlisle Wishard <cwishard@purdue.edu>
2022-11-01 16:07:08 -0400

    added licensing and copyright info to CMake files

--- cmake/Modules/SetCompileFlag.cmake
+++ cmake/Modules/SetCompileFlag.cmake
@@ -1,3 +1,12 @@
+# Copyright 2022 - David Minton, Carlisle Wishard, Jennifer Pouplin, Jake Elliott, & Dana Singh
+# This file is part of Swiftest.
+# Swiftest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Swiftest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Swiftest. 
+# If not, see: https://www.gnu.org/licenses. 
+
 #############################################################################
 # Given a list of flags, this function will try each, one at a time,
 # and choose the first flag that works.  If no flags work, then nothing
