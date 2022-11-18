168d2de5a206d4ac7576f57efa8074070d1a2606
David A Minton <daminton@purdue.edu>
2022-11-17 19:47:51 -0500

    Fixed some inconsistencies with the new API and the id vs name dimensioning

---
+++ examples/helio_gr_test/init_cond.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import swiftest
+
+sim = swiftest.Simulation()
+sim.param['PL_IN'] = "pl.swiftest.in"
+sim.param['TP_IN'] = "tp.swiftest.in"
+sim.param['CB_IN'] = "cb.swiftest.in"
+sim.param['BIN_OUT'] = "bin.swiftest.nc"
+
+sim.param['MU2KG'] = swiftest.MSun
+sim.param['TU2S'] = swiftest.YR2S
+sim.param['DU2M'] = swiftest.AU2M
+sim.param['T0'] = 0.0
+sim.param['DT'] = 0.25 * swiftest.JD2S / swiftest.YR2S
+sim.param['TSTOP'] = 1000.0
+sim.param['ISTEP_OUT']  = 1461
+sim.param['ISTEP_DUMP'] = 1461
+sim.param['CHK_QMIN_COORD'] = "HELIO"
+sim.param['CHK_QMIN'] = swiftest.RSun / swiftest.AU2M
+sim.param['CHK_QMIN_RANGE'] = f"{swiftest.RSun / swiftest.AU2M} 1000.0"
+sim.param['CHK_RMIN'] = swiftest.RSun / swiftest.AU2M
+sim.param['CHK_RMAX'] = 1000.0
+sim.param['CHK_EJECT'] = 1000.0
+sim.param['OUT_STAT'] = "UNKNOWN"
+sim.param['IN_FORM'] = "EL"
+sim.param['OUT_FORM'] = "XVEL"
+sim.param['OUT_TYPE'] = "NETCDF_DOUBLE"
+sim.param['RHILL_PRESENT'] = "YES"
+sim.param['GR'] = 'YES'
+
+bodyid = {
+   "Sun": 0,
+   "Mercury": 1,
+   "Venus": 2,
+   "Earth": 3,
+   "Mars": 4,
+   "Jupiter": 5,
+   "Saturn": 6,
+   "Uranus": 7,
+   "Neptune": 8,
+}
+
+for name, id in bodyid.items():
+   sim.add(name, idval=id, date="2027-04-30")
+   
+sim.save("param.swiftest.in")
+
+
