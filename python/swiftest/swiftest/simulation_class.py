0168fabb135d5d2588c114030a4248260e81af42
David Minton <daminton@purdue.edu>
2022-11-21 11:41:27 -0500

    Added in the curses wrapper

--- python/swiftest/swiftest/simulation_class.py
+++ python/swiftest/swiftest/simulation_class.py
@@ -353,6 +353,45 @@ class Simulation:
                 warnings.warn(f"BIN_OUT file {binpath} not found.",stacklevel=2)
         return
 
+    def _run_swiftest_driver(self):
+        """
+        Internal callable function that executes the swiftest_driver run
+        """
+
+        # Get current environment variables
+
+        env = os.environ.copy()
+        driver_script = os.path.join(self.binary_path, "swiftest_driver.sh")
+        with open(driver_script, 'w') as f:
+            f.write(f"#{self._shell_full} -l\n")
+            f.write(f"source ~/.{self._shell}rc\n")
+            f.write(f"cd {self.sim_dir}\n")
+            f.write(f"{str(self.driver_executable)} {self.integrator} {str(self.param_file)} compact\n")
+
+        cmd = f"{env['SHELL']} -l {driver_script}"
+
+        try:
+            with subprocess.Popen(shlex.split(cmd),
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  env=env,
+                                  universal_newlines=True) as p:
+                for line in p.stdout:
+                    if '[' in line:
+                        print(line.replace('\n', '\r'), end='')
+                    elif "Normal termination" in line:
+                        print(line.replace("Normal termination", "\n\nNormal termination"), end='')
+                    else:
+                        print(line, end='')
+                res = p.communicate()
+                if p.returncode != 0:
+                    for line in res[1]:
+                        print(line, end='')
+                        warnings.warn("Failure in swiftest_driver", stacklevel=2)
+        except:
+            warnings.warn(f"Error executing main swiftest_driver program", stacklevel=2)
+
+        return
 
     def run(self,**kwargs):
         """
@@ -387,39 +426,7 @@ class Simulation:
 
         print(f"Running a {self.codename} {self.integrator} run from tstart={self.param['TSTART']} {self.TU_name} to tstop={self.param['TSTOP']} {self.TU_name}")
 
-        # Get current environment variables
-        env = os.environ.copy()
-        driver_script = os.path.join(self.binary_path,"swiftest_driver.sh")
-        with open(driver_script,'w') as f:
-            f.write(f"#{self._shell_full} -l\n")
-            f.write(f"source ~/.{self._shell}rc\n")
-            f.write(f"cd {self.sim_dir}\n")
-            f.write(f"{str(self.driver_executable)} {self.integrator} {str(self.param_file)} compact\n")
-
-        cmd = f"{env['SHELL']} -l {driver_script}"
-
-        stdscr = curses.initscr()
-        try:
-            with subprocess.Popen(shlex.split(cmd),
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.PIPE,
-                                  env=env,
-                                  universal_newlines=True) as p:
-                for line in p.stdout:
-                    if '[' in line:
-                        print(line.replace('\n','\r'), end='')
-                    elif "Normal termination" in line:
-                        print(line.replace("Normal termination","\n\nNormal termination"),end='')
-                    else:
-                        print(line, end='')
-                res = p.communicate()
-                if p.returncode != 0:
-                    for line in res[1]:
-                       print(line, end='')
-                       warnings.warn("Failure in swiftest_driver", stacklevel=2)
-        except:
-            warnings.warn(f"Error executing main swiftest_driver program",stacklevel=2)
-            return
+        curses.wrapper(self._run_swiftest_driver())
 
         # Read in new data
         self.bin2xr()
