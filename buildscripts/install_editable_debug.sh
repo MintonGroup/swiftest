ba23e4f6416a18e631193c699c69c4c8f5dbf7d1
David A Minton <daminton@purdue.edu>
2023-09-18 11:27:07 -0400

    Fixed issue that was causing the wrong cython executable from being called when building the editable wheel

--- buildscripts/install_editable_debug.sh
+++ buildscripts/install_editable_debug.sh
@@ -8,7 +8,7 @@ cd ${ROOT_DIR}
 python3 -m venv ${VENV_DIR}
 . ${VENV_DIR}/bin/activate
 python3 -m pip install --upgrade pip 
-pip install scikit-build-core pyproject-metadata pathspec ninja 
+pip install scikit-build-core pyproject-metadata pathspec ninja cython
 pip install --config-settings=editable.rebuild=true \
             --config-settings=build-dir="build/{wheel_tag}" \
             --config-settings=cmake.build-type="Debug" \
