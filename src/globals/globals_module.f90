44b4b9135a139979b022b198f08eeae884f6d959
MintoDA1 <51412913+MintoDA1@users.noreply.github.com>
2023-10-02 11:26:58 -0500

    Fixed bad version

--- src/globals/globals_module.f90
+++ src/globals/globals_module.f90
@@ -48,7 +48,7 @@ module globals
    integer(I4B), parameter :: UPPERCASE_OFFSET = iachar('A') - iachar('a') !! ASCII character set parameter for lower to upper 
                                                                            !! conversion - offset between upper and lower
 
-   character(*), parameter :: VERSION = "2023.9.10" !! Swiftest version
+   character(*), parameter :: VERSION = "2023.10.0" !! Swiftest version
 
    !> Symbolic name for integrator types
    character(*), parameter :: UNKNOWN_INTEGRATOR = "UKNOWN INTEGRATOR"
