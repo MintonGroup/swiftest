9d37e11ac2aebee9dea734178a9e99378bac38f4
David Minton <daminton@purdue.edu>
2024-02-13 18:19:17 -0500

    Fixed it so that the release mode version generates the Python install, while the debug version does not

--- buildscripts/build_negishi.sh
+++ buildscripts/build_negishi.sh
@@ -18,23 +18,26 @@ module load use.own
 module load conda-env/mintongroup-py3.9.13
 module load netcdf-fortran/intel-oneapi/4.6.1
 module load shtools/intel-oneapi/4.11.10
-# cmake -P distclean.cmake
-# pip install --config-settings=build-dir="build" \
-#             --config-settings=cmake.build-type="${BUILD_TYPE}" \
-#             --config-settings=cmake.args="-DUSE_SIMD=ON" \
-#             --config-settings=cmake.args="-DUSE_OPENMP=ON" \
-#             --config-settings=cmake.args="-DCMAKE_Fortran_COMPILER=mpiifort" \
-#             --config-settings=cmake.args="-DCMAKE_Fortran_FLAGS=\"-f90=ifort\"" \
-#             --config-settings=cmake.args="-DMACHINE_CODE_VALUE=\"CORE-AVX-I\" " \
-#             --config-settings=install.strip=false \
-#             --no-build-isolation \
-#             -ve . 
-
 cmake -P distclean.cmake
-cmake -B ${ROOT_DIR}/build -S . -G Ninja \
+if [[ BUILD_TYPE == "Release" ]]; then
+    pip install --config-settings=build-dir="build" \
+            --config-settings=cmake.build-type="${BUILD_TYPE}" \
+            --config-settings=cmake.args="-DUSE_SIMD=ON" \
+            --config-settings=cmake.args="-DUSE_OPENMP=ON" \
+            --config-settings=cmake.args="-DCMAKE_Fortran_COMPILER=mpiifort" \
+            --config-settings=cmake.args="-DCMAKE_Fortran_FLAGS=\"-f90=ifort\"" \
+            --config-settings=cmake.args="-DMACHINE_CODE_VALUE=\"CORE-AVX-I\" " \
+            --config-settings=install.strip=false \
+            --no-build-isolation \
+            -ve . 
+else
+    pip uninstall swiftest -y
+    cmake -P distclean.cmake
+    cmake -B ${ROOT_DIR}/build -S . -G Ninja \
     -DMACHINE_CODE_VALUE="CORE-AVX-I" \
     -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
     -DCMAKE_Fortran_COMPILER=mpiifort \
     -DCMAKE_Fortran_FLAGS="-f90=ifort" 
 
-cmake --build ${ROOT_DIR}/build -j${OMP_NUM_THREADS} -v
+    cmake --build ${ROOT_DIR}/build -j${OMP_NUM_THREADS} -v
+fi
