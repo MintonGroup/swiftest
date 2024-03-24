7a9d1a32a67d94b7f12f47a66117a1b8ed0672e1
David Minton <daminton@purdue.edu>
2024-03-24 10:52:38 -0400

    Upgraded mac build to gfortran-13

--- buildscripts/set_compilers.sh
+++ buildscripts/set_compilers.sh
@@ -20,107 +20,39 @@ ROOT_DIR=$(realpath ${SCRIPT_DIR}/..)
 OS=$(uname -s)
 ARCH=$(uname -m)
 
-case $ARCH in
-    x86_64)
-        ;;
-    amd64)
-        ARCH="x86_64"
-        ;;
-    arm64)
-        if [ "$OS" = "Linux" ]; then
-            ARCH="aarch64"
-        fi
-        ;;
-    aarch64)
-        if [ "$OS" = "Darwin" ]; then
-            ARCH="arm64"
-        fi
-        ;;
-    *)
-        echo "Swiftest is currently not configured to build for platform ${OS}-${ARCH}"
-        exit 1
-        ;;
-esac
-
 case $OS in
     Darwin)
         OS="MacOSX" 
-        ;;
-    *MSYS*)
-        OS="Windows"
-        ;;
 esac
 
-if [[ $OS == "Linux" ]]; then
-    # Check if FC is set yet, and if so, use it instead of the default
-    # Currently ifx support is not great
-    case $FC in
-    *ifx) 
-        OS="Linux-ifx" 
-        ;;
-    *mpiifort)
-        OS="Linux-mpiifort"
-        ;;
-    *ifort)
-        OS="Linux-ifort"
-        ;;
-    *gfortran)
-        OS="Linux-gnu"
-        ;;
-    *)
-        OS="Linux-gnu"
-        ;;
-    esac
-fi 
 set -a
+# Set default compilers if they are not already set
 case $OS in
-    Linux-gnu)
+    Linux)
         FC=${FC:-$(command -v gfortran)}
-        CC=$(command -v gcc)
-        CXX=$(command -v g++)
-        CPP=$(command -v cpp)
-        ;;
-    Linux-ifx)
-        FC=$(command -v ifx)
-        CC=$(command -v icx)
-        CXX=$(command -v icpx)
-        CPP=$(command -v cpp)
-        ;;
-    Linux-ifort)
-        FC=$(command -v ifort)
-        CC=$(command -v icc)
-        CXX=$(command -v icpc)
-        CPP=$(command -v cpp)
-        ;;
-    Linux-mpiifort)
-        FC=$(command -v mpiifort)
-        CC=$(command -v mpiicc)
-        CXX=$(command -v mpiicpc)
-        CPP=$(command -v cpp)
+        CC=${CC:-$(command -v gcc)}
+        CXX=${CXX:-$(command -v g++)}
+        CPP=${CPP:-$(command -v cpp)}
+        AR=${AR:-$(command -v ar)}
+        NM=${NM:-$(command -v nm)}
+        RANLIB=${RANLIB:-$(command -v ranlib)}
         ;;
     MacOSX)
-        FC=${HOMEBREW_PREFIX}/bin/gfortran-12
-        CFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -Wno-deprecated-non-prototype -arch ${ARCH}"
-        FCFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -arch ${ARCH}"
-        FFLAGS=$FCFLAGS
-        LD_LIBRARY_PATH=""
-        CPATH=""
-        COMPILER_PREFIX="/usr"
-        CC=${COMPILER_PREFIX}/bin/clang
-        CXX=${COMPILER_PREFIX}/bin/clang++
-        CPP=${COMPILER_PREFIX}/bin/cpp
-        AR=${COMPILER_PREFIX}/bin/ar
-        NM=${COMPILER_PREFIX}/bin/nm
-        RANLIB=${COMPILER_PREFIX}/bin/ranlib
-        LDFLAGS="-Wl,-no_compact_unwind"
+        FC=${FC:-"${HOMEBREW_PREFIX}/bin/gfortran-12"}
+        CC=${CC:-"/usr/bin/clang"}
+        CXX=${CXX:-"/usr/bin/clang++"}
+        CPP=${CPP:-"/usr/bin/cpp"}
+        AR=${AR:-"/usr/bin/ar"}
+        NM=${NM:-"/usr/bin/nm"}
+        RANLIB=${RANLIB:-"/bin/ranlib"}
         ;;
-
     *)
         printf "Unknown compiler type: ${OS}\n"
-        echo "Valid options are Linux-gnu, Linux-ifort, Linux-ifx, or MacOSX"
+        echo "Valid options are Linux, Darwin, MacOSX"
         printf $USTMT
         exit 1
         ;;
 esac
+
 F77=${FC}
 F95=${FC}
