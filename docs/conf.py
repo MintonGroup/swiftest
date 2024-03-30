05ffdc6fc4e71303c18e080a496aaa48c3fed42a
David Minton <daminton@purdue.edu>
2024-03-30 09:01:17 -0400

    Updated doc generation options to get figures to show on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,6 @@ templates_path = ["_templates"]
 
 html_theme = 'sphinx_book_theme'
 html_title =""
-html_static_path = ["_static"]
 
 html_context = {
     "github_user": "profminton",
@@ -183,3 +182,6 @@ def html_page_context(app, pagename, templatename, context, doctree):
     # Disable edit button for docstring generated pages
     if "generated" in pagename:
         context["theme_use_edit_page_button"] = False
+        
+def setup(app: Sphinx):
+    app.connect("html-page-context", html_page_context)
