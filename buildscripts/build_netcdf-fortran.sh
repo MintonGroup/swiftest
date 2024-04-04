dba92e53b21da4f9877fc8aeff673cd533613889
David Minton <daminton@purdue.edu>
2024-04-04 09:17:36 -0400

    Updated headers of all build scripts and improved robustness of the MACOX_DEPLOYMENT_TARGET versioning with a dedicated script

--- buildscripts/build_netcdf-fortran.sh
+++ buildscripts/build_netcdf-fortran.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# This script will build all of the dependency libraries needed by Swiftest. Builds the following from source:
-# Zlib, hdf5, netcdf-c, netcdf-fortran
+# This script will the NetCDF Fortran library from source
 # 
-# Copyright 2023 - David Minton
+# Copyright 2024 - The Minton Group at Purdue University
 # This file is part of Swiftest.
 # Swiftest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
 # as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
