4603b0c542d3dc037b2b72088530f61f7cbfd496
David A Minton <daminton@purdue.edu>
2024-04-23 16:29:40 -0400

    Changed the compilers to the OMPI_FC variable rather than the wrapper, because "mpifort" confused the SHTOOLS makefile.

--- buildscripts/build_shtools.sh
+++ buildscripts/build_shtools.sh
@@ -45,13 +45,13 @@ cd ${DEPENDENCY_DIR}/SHTOOLS*
 case $FC in
     *"gfortran"*|*"mpifort")
         echo "Everything else"
-        make -j${NPROC} F95="${FC}" CXX="${CXX}" F95FLAGS="-fPIC -O3 -std=gnu -ffast-math ${FFLAGS}" fortran
-        make -j${NPROC} F95="${FC}" CXX="${CXX}" F95FLAGS="-fPIC -O3 -std=gnu -ffast-math ${FFLAGS}" fortran-mp
+        make -j${NPROC} F95="${OMPI_FC}" CXX="${CXX}" F95FLAGS="-fPIC -O3 -std=gnu -ffast-math ${FFLAGS}" fortran
+        make -j${NPROC} F95="${OMPI_FC}" CXX="${CXX}" F95FLAGS="-fPIC -O3 -std=gnu -ffast-math ${FFLAGS}" fortran-mp
         ;;
     *"mpiifort"*|*"ifx"*)
         echo "Using Intel Fortran compiler"
-        make -j${NPROC} F95="${FC}" CXX="${CXX}" F95FLAGS="-fPIC -m64 -fpp -free -O3 ${FFLAGS} -Tf" fortran
-        make -j${NPROC} F95="${FC}" CXX="${CXX}" F95FLAGS="-fPIC -m64 -fpp -free -O3 ${FFLAGS} -Tf" fortran-mp
+        make -j${NPROC} F95="${OMPI_FC}" CXX="${CXX}" F95FLAGS="-fPIC -m64 -fpp -free -O3 ${FFLAGS} -Tf" fortran
+        make -j${NPROC} F95="${OMPI_FC}" CXX="${CXX}" F95FLAGS="-fPIC -m64 -fpp -free -O3 ${FFLAGS} -Tf" fortran-mp
         ;;
 esac
 
