7a01ebd670233c983a3a992aec96029232b3ba48
David A Minton <daminton@purdue.edu>
2024-04-23 15:20:22 -0400

    More changes to get the compiler set correctly

--- buildscripts/get_gfortran_version.sh
+++ buildscripts/get_gfortran_version.sh
@@ -11,6 +11,7 @@
 # You should have received a copy of the GNU General Public License along with Swiftest. 
 # If not, see: https://www.gnu.org/licenses. 
 
+OMPI_FC="$(command -v gfortran-13 || command -v gfortran-12 || command -v gfortran)"
 FC=${FC:-"$(command -v mpif90)"}
 echo "$(${FC} --version | head -n1 | awk '{print $NF}' | cut -d. -f1)"
 set +a
