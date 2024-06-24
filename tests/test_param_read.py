bdd752b96a753b4860cd2dae843299be5bc14126
Kaustub Parvir Anand <anand43@b000.negishi.rcac.purdue.edu>
2024-06-24 13:35:10 -0400

    added unit test

---
+++ tests/test_param_read.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+ Copyright 2024 - The Minton Group at Purdue University
+ This file is part of Swiftest.
+ Swiftest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ Swiftest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with Swiftest. 
+ If not, see: https://www.gnu.org/licenses. 
+"""
+import swiftest
+import unittest
+import os
+import tempfile
+import astropy.constants as const
+
+class TestSwiftestUnits(unittest.TestCase):
+    def setUp(self):
+        # Initialize a target and surface for testing
+        self.tmpdir=tempfile.TemporaryDirectory()
+        self.simdir = self.tmpdir.name
+        
+    def tearDown(self):
+        # Clean up temporary directory
+        self.tmpdir.cleanup() 
+
+    def test_param_read(self):
+        """
+        Test that the user defined param_file is read correctly
+        """
+
+        user_param_file = 'user_param.in'
+
+        sim = swiftest.Simulation(simdir = self.simdir, param_file = user_param_file)
+
+        self.assertEqual(str(sim.param_file), user_param_file)
+    
+    def test_param_read_restart(self):
+        """
+        Test that a param.restart.in file is read correctly
+        """
+
+        user_param_file = 'param.in'
+        sim = swiftest.Simulation(simdir = self.simdir, integrator = 'symba', 
+                                read_param = True, param_file = user_param_file,
+                                dump_cadence = 1, tstep_out = 0.5, 
+                                tstop = 1, dt = 0.01)
+        sim.clean()
+
+        sim.add_solar_system_body(['Sun', 'Earth', 'Mars'])
+
+        sim.run()
+
+        # restart run as a second check
+        restarted_param_file = 'param.restart.in'
+        sim_restart = swiftest.Simulation(simdir = self.simdir, 
+                                        read_param = True, read_collisions=True, read_data = True,
+                                        param_file = restarted_param_file)
+
+        self.assertEqual(str(sim_restart.param_file), restarted_param_file)
+
+if __name__ == '__main__':
+    os.environ["HDF5_USE_FILE_LOCKING"]="FALSE"
+    unittest.main()
+
+
+
+
+
+
+
+
