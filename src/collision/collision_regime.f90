6df0c3705e6242561d1bc66bf9d4465590b498cb
Kaustub Parvir Anand <anand43@b000.negishi.rcac.purdue.edu>
2024-07-09 16:21:25 -0400

    fixed typo

--- src/collision/collision_regime.f90
+++ src/collision/collision_regime.f90
@@ -292,7 +292,7 @@ contains
       mu_HG20_imp = a_imp * theta**2 + b_imp * theta + c_imp
       C_HG20_imp = exp(d_imp * theta**3 + e_imp * theta**2 + f_imp * theta + g_imp) 
       M_esc_imp = C_HG20_imp * (V_esc / (V_imp * sin(theta_rad)))**(-3.0_DP * mu_HG20_imp) ! impactor mass units
-      M_esc_imp = max(M_esc_imp, 1.0_DP) ! Max value of M_esc_imp is 1.0 (impactor mass units)
+      M_esc_imp = min(M_esc_imp, 1.0_DP) ! Max value of M_esc_imp is 1.0 (impactor mass units)
 
       M_esc_total = M_esc_tar + M_esc_imp ! impactor mass units
 
