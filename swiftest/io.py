Fixed a number of issues involving floating point precision. Now the unit conversion factors are stored as np.longdouble type. On the fortran side, mass values are read in if they exist in the input file rather than computing them from Gmass

swiftest/io.py
@@ -59,13 +59,13 @@ bool_param = ["RESTART",
               "COARRAY"]
 
 int_param = ["ISTEP_OUT", "DUMP_CADENCE"]
-float_param = ["T0", "TSTART", "TSTOP", "DT", "CHK_RMIN", "CHK_RMAX", "CHK_EJECT", "CHK_QMIN", "DU2M", "MU2KG",
-               "TU2S", "MIN_GMFRAG", "GMTINY"]
+float_param = ["T0", "TSTART", "TSTOP", "DT", "CHK_RMIN", "CHK_RMAX", "CHK_EJECT", "CHK_QMIN", "MIN_GMFRAG", "GMTINY"]
+quad_param = ["DU2M", "MU2KG", "TU2S"]
 
 upper_str_param = ["OUT_TYPE","OUT_FORM","OUT_STAT","IN_TYPE","IN_FORM","ENCOUNTER_SAVE", "CHK_QMIN_COORD"]
 lower_str_param = ["NC_IN", "PL_IN", "TP_IN", "CB_IN", "CHK_QMIN_RANGE"]
 
-param_keys = ['! VERSION'] + int_param + float_param + upper_str_param + lower_str_param+ bool_param
+param_keys = ['! VERSION'] + int_param + float_param + quad_param + upper_str_param + lower_str_param+ bool_param
 
 # This defines SwiftestDataset variables that are strings, which must be processed due to quirks in how NetCDF-Fortran
 # handles strings differently than Python's Xarray.
@@ -142,7 +142,7 @@ def _str2bool(input_str):
 
 def _real2float(realstr):
     """
-    Converts a Fortran-generated ASCII string of a real value into a numpy float type. Handles cases where double precision
+    Converts a Fortran-generated ASCII string of a real value into a numpy float64 type. Handles cases where double precision
     numbers in exponential notation use 'd' or 'D' instead of 'e' or 'E'
     
     Parameters
@@ -155,7 +155,24 @@ def _real2float(realstr):
     float
         The converted floating point value of the input string
     """
-    return float(realstr.replace('d', 'E').replace('D', 'E'))
+    return np.float64(realstr.replace('d', 'E').replace('D', 'E'))
+
+def _real2quad(realstr):
+    """
+    Converts a Fortran-generated ASCII string of a real value into a numpy longdouble type. Handles cases where double precision 
+    numbers in exponential notation use 'd' or 'D' instead of 'e' or 'E'
+    
+    Parameters
+    ----------
+    realstr : str
+        Fortran-generated ASCII string of a real value.
+        
+    Returns
+    -------
+    np.longdouble
+        The converted floating point value of the input string
+    """ 
+    return np.longdouble(realstr.replace('d', 'E').replace('D', 'E'))
 
 
 def read_swiftest_param(param_file_name: os.PathLike, param: dict, verbose: bool=True) -> dict:
@@ -212,6 +229,10 @@ def read_swiftest_param(param_file_name: os.PathLike, param: dict, verbose: bool
         for f in float_param:
             if f in param and type(param[f]) is str:
                 param[f] = _real2float(param[f])
+        
+        for q in quad_param:
+            if q in param and type(param[q]) is str:
+                param[q] = _real2quad(param[q])
 
         for b in bool_param:
             if b in param:
@@ -462,6 +483,10 @@ def write_labeled_param(param: dict, param_file_name: os.PathLike) -> None:
     Returns
     -------
     None
+    
+    Notes
+    -----
+    Using str(val) instead of f-strings prevents np.longdouble from being cast as float before printing.
     """
     outfile = open(param_file_name, 'w')
     ptmp = param.copy()
@@ -472,10 +497,10 @@ def write_labeled_param(param: dict, param_file_name: os.PathLike) -> None:
             if type(val) is bool:
                 print(f"{key:<32} {_bool2yesno(val)}", file=outfile)
             else:
-                print(f"{key:<32} {val}", file=outfile)
+                print(f"{key:<32} "+str(val), file=outfile)
     val = ptmp.pop('SEED', None)
     if val is not None:
-        seed_str = f"{val.size} " + " ".join(map(str, val))
+        seed_str = f"{val.size} "+ " ".join(map(str, val))
         print(f"{'SEED':<32} {seed_str}", file=outfile)
 
     # Print the remaining key/value pairs in whatever order
@@ -484,7 +509,7 @@ def write_labeled_param(param: dict, param_file_name: os.PathLike) -> None:
             if type(val) is bool:
                 print(f"{key:<32} {_bool2yesno(val)}", file=outfile)
             else:
-                print(f"{key:<32} {val}", file=outfile)
+                print(f"{key:<32} "+str(val), file=outfile)
     outfile.close()
     return
 
@@ -1536,23 +1561,23 @@ def swifter2swiftest(swifter_param: dict,
         print("Unit system is MSun-AU-day")
         swiftest_param['MU2KG'] = MSun
         swiftest_param['DU2M'] = AU2M
-        swiftest_param['TU2S'] = JD2S
+        swiftest_param['TU2S'] = np.longdouble(JD2S)
     elif unit_type == 3 or unit_system.upper() == 'SI':
         print("Unit system is SI: kg-m-s")
-        swiftest_param['MU2KG'] = 1.0
-        swiftest_param['DU2M'] = 1.0
-        swiftest_param['TU2S'] = 1.0
+        swiftest_param['MU2KG'] = np.longdouble(1.0)
+        swiftest_param['DU2M'] = np.longdouble(1.0)
+        swiftest_param['TU2S'] = np.longdouble(1.0)
     elif unit_type == 4 or unit_system.upper() == 'CGS':
         print("Unit system is CGS: g-cm-s")
-        swiftest_param['MU2KG'] = 1e-3
-        swiftest_param['DU2M'] = 1.0e-2
-        swiftest_param['TU2S'] = 1.0
+        swiftest_param['MU2KG'] = np.longdouble(1e-3)
+        swiftest_param['DU2M'] = np.longdouble(1.0e-2)
+        swiftest_param['TU2S'] = np.longdouble(1.0)
     elif unit_type == 5:
         print("User-defined units.")
         print("Define each unit (mass, distance, and time) by its corresponding SI value.")
-        swiftest_param['MU2KG'] = input("Mass value in kilograms: ")
-        swiftest_param['DU2M'] = input("Distance value in meters: ")
-        swiftest_param['TU2S'] = input("Time unit in seconds: ")
+        swiftest_param['MU2KG'] = np.longdouble(input("Mass value in kilograms: "))
+        swiftest_param['DU2M'] = np.longdouble(input("Distance value in meters: "))
+        swiftest_param['TU2S'] = np.longdouble(input("Time unit in seconds: "))
     GU = GC / (swiftest_param['DU2M'] ** 3 / (swiftest_param['MU2KG'] * swiftest_param['TU2S'] ** 2))
     print(f"Central body mass: {GMcb / GU} MU ({(GMcb / GU) * swiftest_param['MU2KG']} kg)")
    

swiftest/simulation.py
@@ -149,7 +149,7 @@ class Simulation(object):
         self._codename = None
         self._integrator = None
         self._restart = False
-        
+
         # Define a dictionary that maps parameter names used in `param.in` files to arguments passed to Simulation() and `set_parameter()`
         self._param_to_argument = {
             "T0": "t0",
@@ -240,9 +240,26 @@ class Simulation(object):
             "ENCOUNTER_SAVE" : "NONE",
             "TIDES": False,
         }         
+        
         self.codename = codename
         self.integrator = integrator
         self.simdir = simdir
+        
+        if read_init_cond is None:
+            read_init_cond = read_data 
+        
+        if read_collisions is None:
+            read_collisions = read_data
+        
+        if read_encounters is None:
+            read_encounters = read_data
+            
+        if read_init_cond or read_data or read_collisions or read_encounters:
+            read_param = True        
+            
+        if clean and not read_param:
+            self.clean(deep=True)
+        
         self.param_file = param_file
 
         # Parameters are set in reverse priority order. First the defaults, then values from a pre-existing input file,
@@ -259,22 +276,7 @@ class Simulation(object):
 
         # If the user asks to read in an old parameter file or output file, override any default parameters with values from the file
         # If the file doesn't exist, flag it for now so we know to create it
-        param_file_found = False
-        
-        if read_init_cond is None:
-            read_init_cond = read_data 
-        
-        if read_collisions is None:
-            read_collisions = read_data
-        
-        if read_encounters is None:
-            read_encounters = read_data
-            
-        if read_init_cond or read_data or read_collisions or read_encounters:
-            read_param = True
-            
-        if clean and not read_param:
-            self.clean(deep=True)
+
             
         if read_param: 
             self.read_param(**kwargs)
@@ -4568,7 +4570,7 @@ class Simulation(object):
     def MU2KG(self, value: FloatLike) -> None:
         if not isinstance(value, (float, int, np.number)):
             raise TypeError("Mass unit to kilogram conversion value must be a float")
-        self._MU2KG = np.float64(value)
+        self._MU2KG = np.longdouble(value)
         self._KG2MU = 1.0 / self._MU2KG
         return 
    
@@ -4583,7 +4585,7 @@ class Simulation(object):
     def KG2MU(self, value: FloatLike) -> None:
         if not isinstance(value, (float, int, np.number)):
             raise TypeError("Kilogram to mass unit conversion value must be a float")
-        self._KG2MU = np.float64(value)
+        self._KG2MU = np.longdouble(value)
         self._MU2KG = 1.0 / self._KG2MU
         return 
     
@@ -4598,7 +4600,7 @@ class Simulation(object):
     def DU2M(self, value: FloatLike) -> None:
         if not isinstance(value, (float, int, np.number)):
             raise TypeError("Distance unit to meter conversion value must be a float")
-        self._DU2M = np.float64(value)
+        self._DU2M = np.longdouble(value)
         self._M2DU = 1.0 / self._DU2M
         return
     
@@ -4613,7 +4615,7 @@ class Simulation(object):
     def M2DU(self, value: FloatLike) -> None:
         if not isinstance(value, (float, int, np.number)):
             raise TypeError("Meter to distance unit conversion value must be a float")
-        self._M2DU = np.float64(value)
+        self._M2DU = np.longdouble(value)
         self._DU2M = 1.0 / self._M2DU
         return
     
@@ -4628,7 +4630,7 @@ class Simulation(object):
     def TU2S(self, value: FloatLike) -> None:
         if not isinstance(value, (float, int, np.number)):
             raise TypeError("Time unit to second conversion value must be a float")
-        self._TU2S = np.float64(value)
+        self._TU2S = np.longdouble(value)
         self._S2TU = 1.0 / self._TU2S
         return
     
@@ -4643,7 +4645,7 @@ class Simulation(object):
     def S2TU(self, value: FloatLike) -> None:
         if not isinstance(value, (float, int, np.number)):
             raise TypeError("Second to time unit conversion value must be a float")
-        self._S2TU = np.float64(value)
+        self._S2TU = np.longdouble(value)
         self._TU2S = 1.0 / self._S2TU
         return
    