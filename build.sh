9508eb11544bb5a75f1ab15ecdfbcae45f56e422
David Minton <daminton@purdue.edu>
2024-11-22 08:08:18 -0500

    Removed the build file because it's not working as expected. I need to find a better way to make it work with conda

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SCRIPT_DIR=$(realpath "buildscripts")
-ROOT_DIR=$(realpath $(dirname $0))
-
-cd $SCRIPT_DIR
-. set_environment.sh
-./build_dependencies.sh
-cd $ROOT_DIR
-python -m pip install . --no-deps -vv
