d2f9370a3219061a5830249b9e0c4939cf9f31dc
David Minton <daminton@purdue.edu>
2025-12-10 13:59:37 -0800

    Added constructors and destructors for ringmoons objects

--- src/ringmoons/ringmoons_module.f90
+++ src/ringmoons/ringmoons_module.f90
@@ -8,66 +8,191 @@
 ! If not, see: https://www.gnu.org/licenses. 
 
 module ringmoons
-   !! author: Andrew Hesselbrock and David A. Minton
-   !!
-   !! Definition of classes and methods specific to the Ringmoons integrator
-   use swiftest
-   use symba
-   implicit none
-   public
+    !! author: Andrew Hesselbrock and David A. Minton
+    !!
+    !! Definition of classes and methods specific to the Ringmoons integrator
+    use swiftest
+    use symba
+    implicit none
+    public
 
-   !> Ringmoons central body particle class
-   type, extends(symba_cb) :: ringmoons_cb
-   end type ringmoons_cb
-
-
-   !> Ringmoons massive body class
-   type, extends(symba_pl) :: ringmoons_pl
-   end type ringmoons_pl
+    !> Ringmoons central body particle class
+    type, extends(symba_cb) :: ringmoons_cb
+    end type ringmoons_cb
 
 
-   !> Ringmoons test particle class
-   type, extends(symba_tp) :: ringmoons_tp
-   contains
-   end type ringmoons_tp
+    !> Ringmoons massive body class
+    type, extends(symba_pl) :: ringmoons_pl
+    end type ringmoons_pl
 
-   type, extends(base_object) :: ringmoons_ring
-   contains
-      procedure:: dealloc  => ringmoons_util_dealloc_ring
-   end type ringmoons_ring
 
-   type, extends(base_object) :: ringmoons_seeds
-   contains
-      procedure:: dealloc  => ringmoons_util_dealloc_seeds
-   end type ringmoons_seeds
+    !> Ringmoons test particle class
+    type, extends(symba_tp) :: ringmoons_tp
+    contains
+    end type ringmoons_tp
 
-   type, extends(symba_nbody_system) :: ringmoons_nbody_system
-      class(ringmoons_ring),         allocatable :: ring
-         !! Ringmoons ring object
-      class(ringmoons_seeds),        allocatable :: seeds
-         !! Ringmoons seeds object
-   end type ringmoons_nbody_system
+    type, extends(base_object) :: ringmoons_ring
+        integer(I4B) :: nbins               
+            !! number of bins in ring
+        integer(I4B) :: inside = 1          
+            !! bin id of innermost ring bin (can increase if primary accretes a lot mass through updates)
+        real(DP)     :: r_F                 
+            !! outside radius of ring in simulation length units
+        real(DP)     :: X_F                 
+            !! outside radius of ring in X units (see Bath & Pringle 1981)
+        real(DP)     :: r_I                 
+            !! inside radius of ring in simulation length units
+        real(DP)     :: X_I                 
+            !! inside radius of ring in X units (see Bath & Pringle 1981)
+        real(DP)     :: deltaX              
+            !! variable changed bin width used for viscosity calculations in X units
+        real(DP)     :: RRL,FRL             
+            !! Rigid and fluid Roche limits
+        integer(I4B) :: iRRL,iFRL           
+            !! Indexes of Roche limit bins
+        real(DP), dimension(:), allocatable :: r                 
+            !! radial distance of center of bin in simulation length units
+        real(DP), dimension(:), allocatable :: r_hstar           
+            !! normalized ring Hill's radius in simulation length units
+        real(DP), dimension(:), allocatable :: X                 
+            !! distance variable X bin center used for viscosity calculations
+        real(DP), dimension(:), allocatable :: X2                
+            !! distance variable X**2 bin center  used for viscosity calculations
+        real(DP), dimension(:), allocatable :: deltaA            
+            !! differential surface area of ring
+        real(DP), dimension(:), allocatable :: mass              
+            !! mass of ring particles in bin
+        real(DP), dimension(:), allocatable :: sigma 
+            !! surface mass density of ring bin
+        real(DP), dimension(:), allocatable :: tau               
+            !! ring optical depth
+        real(DP), dimension(:), allocatable :: nu                
+            !! viscocity of the ring bin
+        real(DP), dimension(:), allocatable :: Q                 
+            !! Toomre parameter of the ring bin
+        real(DP), dimension(:), allocatable :: Iz                
+            !! polar moment of inertia of ring bin
+        real(DP), dimension(:), allocatable :: w                 
+            !! Keplerian angular velocity of ring bin
+        real(DP), dimension(:), allocatable :: Torque            
+            !! total satellite torque density acting on the ring bin
+        real(DP), dimension(:), allocatable :: r_p
+            !! ring particle radius
+        real(DP), dimension(:), allocatable :: m_p
+            !! ring particle mass
+        real(DP), dimension(:), allocatable :: rho_p
+            !! ring particle mass density
+        real(DP), dimension(:), allocatable :: vrel_p
+            !! ring particle relative velocity
+    contains
+        procedure :: setup    => ringmoons_util_setup_ring
+        procedure :: dealloc  => ringmoons_util_dealloc_ring
+        final     ::             ringmoons_final_ring
+            !! Finalizes the ringmoons ring object - deallocates all allocatables
+    end type ringmoons_ring
 
-   interface
-      module subroutine ringmoons_util_dealloc_ring(self)
-         !! author: David A. Minton
-         !!
-         !! Deallocates all allocatabale arrays
-         implicit none
-         ! Arguments
-         class(ringmoons_ring),  intent(inout) :: self 
+    type, extends(base_object) :: ringmoons_seeds
+        integer(I4B)                              :: nbody            
+            !! Number of satellite seeds
+        real(DP)                                  :: feeding_zone_factor 
+            !! Width of feeding zone for seed mergers in units of mutual Hill's sphere
+        real(DP)                                  :: rkf_tol      
+            !! Error tolerance for Runge-Kutta-Fehlberg integrator for seed evolution
+        real(DP)                                  :: mass_init       
+            !! initial mass of seeds
+        logical, dimension(:), allocatable   :: active       
+            !! Flag to determine whether this body is active or not
+        real(DP), dimension(:), allocatable       :: a            
+            !! Semimajor axis of seed
+        real(DP), dimension(:), allocatable       :: mass           
+            !! Mass of seed
+        real(DP), dimension(:), allocatable       :: Rhill        
+            !! Hill's sphere radius of seed
+        integer(I4B), dimension(:), allocatable   :: rbin         
+            !! Ring bin location of seed
+        real(DP), dimension(:), allocatable       :: Torque       
+            !! Total torque acting on the seed
+        real(DP), dimension(:), allocatable       :: Ttide        
+            !! Tidal torque acting on the seed
+    contains
+        procedure :: setup    => ringmoons_util_setup_seeds
+        procedure :: dealloc  => ringmoons_util_dealloc_seeds
+        final     ::             ringmoons_final_seeds
+            !! Finalizes the ringmoons seeds object - deallocates all allocatables
+    end type ringmoons_seeds
+
+    type, extends(symba_nbody_system) :: ringmoons_nbody_system
+        class(ringmoons_ring),         allocatable :: ring
             !! Ringmoons ring object
-      end subroutine ringmoons_util_dealloc_ring
+        class(ringmoons_seeds),        allocatable :: seeds
+            !! Ringmoons seeds object
+    end type ringmoons_nbody_system
 
-      module subroutine ringmoons_util_dealloc_seeds(self)
-         !! author: David A. Minton
-         !!
-         !! Deallocates all allocatabale arrays
-         implicit none
-         ! Arguments
-         class(ringmoons_seeds),  intent(inout) :: self 
-            !! Ringmoons ring object
-      end subroutine ringmoons_util_dealloc_seeds
-   end interface
+    interface
+        module subroutine ringmoons_util_dealloc_ring(self)
+            !! author: David A. Minton
+            !!
+            !! Deallocates all allocatabale arrays
+            implicit none
+            ! Arguments
+            class(ringmoons_ring),  intent(inout) :: self 
+                !! Ringmoons ring object
+        end subroutine ringmoons_util_dealloc_ring
+
+        module subroutine ringmoons_util_dealloc_seeds(self)
+            !! author: David A. Minton
+            !!
+            !! Deallocates all allocatabale arrays
+            implicit none
+            ! Arguments
+            class(ringmoons_seeds),  intent(inout) :: self 
+                !! Ringmoons ring object
+            end subroutine ringmoons_util_dealloc_seeds
+
+        module subroutine ringmoons_util_setup_ring(self, n, param)
+            implicit none
+            class(ringmoons_ring),      intent(inout) :: self  
+                !! Ringmoons ring object
+            integer(I4B),               intent(in)    :: n     
+                !! Number of bins to allocate space for
+            class(swiftest_parameters), intent(in)    :: param 
+                !! Current run configuration parameters
+        end subroutine ringmoons_util_setup_ring
+
+        module subroutine ringmoons_util_setup_seeds(self, n, param)
+            implicit none
+            class(ringmoons_seeds),     intent(inout) :: self  
+                !! Ringmoons seeds object
+            integer(I4B),               intent(in)    :: n     
+                !! Number of bins to allocate space for
+            class(swiftest_parameters), intent(in)    :: param 
+                !! Current run configuration parameters
+        end subroutine ringmoons_util_setup_seeds
+    end interface
+
+    contains
+        subroutine ringmoons_final_ring(self)
+            !! author: David A. Minton
+            !!
+            !! Finalize the ringmoons ring object - deallocates all allocatables
+            implicit none
+            ! Argument
+            type(ringmoons_ring),  intent(inout) :: self 
+                !! Ringmoons ring object
+            call self%dealloc()
+            return
+        end subroutine ringmoons_final_ring
+
+        subroutine ringmoons_final_seeds(self)
+            !! author: David A. Minton
+            !!
+            !! Finalize the ringmoons seeds object - deallocates all allocatables
+            implicit none
+            ! Argument
+            type(ringmoons_seeds),  intent(inout) :: self
+                !! Ringmoons seeds object
+            call self%dealloc()
+            return
+        end subroutine ringmoons_final_seeds
 
 end module ringmoons
--- src/ringmoons/ringmoons_util.f90
+++ src/ringmoons/ringmoons_util.f90
@@ -8,7 +8,7 @@
 ! If not, see: https://www.gnu.org/licenses. 
 
 submodule(ringmoons) s_ringmoons_util
-   use swiftest
+    use swiftest
 contains
    
     module subroutine ringmoons_util_dealloc_ring(self)
@@ -18,7 +18,25 @@ contains
         implicit none
         ! Arguments
         class(ringmoons_ring),  intent(inout) :: self 
-        !! Ringmoons ring object
+            !! Ringmoons ring object
+
+        self%nbins = 0
+        if (allocated(self%r))          deallocate(self%r)
+        if (allocated(self%X))          deallocate(self%X)
+        if (allocated(self%X2))         deallocate(self%X2)
+        if (allocated(self%r_hstar))    deallocate(self%r_hstar)
+        if (allocated(self%deltaA))     deallocate(self%deltaA)
+        if (allocated(self%sigma))      deallocate(self%sigma)
+        if (allocated(self%tau))        deallocate(self%tau)
+        if (allocated(self%nu))         deallocate(self%nu)
+        if (allocated(self%Q))          deallocate(self%Q)
+        if (allocated(self%Iz))         deallocate(self%Iz)
+        if (allocated(self%w))          deallocate(self%w)
+        if (allocated(self%Torque))     deallocate(self%Torque)
+        if (allocated(self%r_p))       deallocate(self%r_p)
+        if (allocated(self%m_p))       deallocate(self%m_p)
+        if (allocated(self%rho_p))     deallocate(self%rho_p)
+        if (allocated(self%vrel_p))    deallocate(self%vrel_p)
 
         return
     end subroutine ringmoons_util_dealloc_ring
@@ -30,8 +48,115 @@ contains
         implicit none
         ! Arguments
         class(ringmoons_seeds),  intent(inout) :: self 
-        !! Ringmoons ring object
+            !! Ringmoons ring object
+
+        self%nbody = 0
+        if (allocated(self%active))    deallocate(self%active)
+        if (allocated(self%a))         deallocate(self%a)
+        if (allocated(self%mass))      deallocate(self%mass)
+        if (allocated(self%Rhill))     deallocate(self%Rhill)
+        if (allocated(self%rbin))      deallocate(self%rbin)
+        if (allocated(self%Torque))    deallocate(self%Torque)
+        if (allocated(self%Ttide))     deallocate(self%Ttide)
 
         return
     end subroutine ringmoons_util_dealloc_seeds
+
+
+    module subroutine ringmoons_util_setup_ring(self, n, param)
+        !! author: David A. Minton
+        !!
+        !! Constructor for the ringmoons_ring class. 
+        !! Allocates space for all bins and initializes all components with a value. 
+        implicit none
+        class(ringmoons_ring),      intent(inout) :: self  
+            !! Ringmoons ring object
+        integer(I4B),               intent(in)    :: n     
+            !! Number of bins to allocate space for
+        class(swiftest_parameters), intent(in)    :: param 
+            !! Current run configuration parameters
+
+        if (n < 0) return
+
+        call self%dealloc()
+        self%nbins = n
+
+        if (n == 0) return
+
+        allocate(self%r(n))
+        allocate(self%X(n))
+        allocate(self%X2(n))
+        allocate(self%r_hstar(n))
+        allocate(self%deltaA(n))
+        allocate(self%sigma(n))
+        allocate(self%tau(n))
+        allocate(self%nu(n))
+        allocate(self%Q(n))
+        allocate(self%Iz(n))
+        allocate(self%w(n))
+        allocate(self%Torque(n))
+        allocate(self%r_p(n))
+        allocate(self%m_p(n))
+        allocate(self%rho_p(n))
+        allocate(self%vrel_p(n))
+
+        self%r(:) = 0.0_DP
+        self%X(:) = 0.0_DP
+        self%X2(:) = 0.0_DP
+        self%r_hstar(:) = 0.0_DP
+        self%deltaA(:) = 0.0_DP
+        self%sigma(:) = 0.0_DP
+        self%tau(:) = 0.0_DP
+        self%nu(:) = 0.0_DP
+        self%Q(:) = 0.0_DP
+        self%Iz(:) = 0.0_DP
+        self%w(:) = 0.0_DP
+        self%Torque(:) = 0.0_DP
+        self%r_p(:) = 0.0_DP
+        self%m_p(:) = 0.0_DP
+        self%rho_p(:) = 0.0_DP
+        self%vrel_p(:) = 0.0_DP
+
+        return
+    end subroutine ringmoons_util_setup_ring
+
+    module subroutine ringmoons_util_setup_seeds(self, n, param)
+        !! author: David A. Minton
+        !!
+        !! Constructor for the ringmoons_seeds class. 
+        !! Allocates space for all bins and initializes all components with a value. 
+        implicit none
+        class(ringmoons_seeds),     intent(inout) :: self  
+            !! Ringmoons seeds object
+        integer(I4B),               intent(in)    :: n     
+            !! Number of bins to allocate space for
+        class(swiftest_parameters), intent(in)    :: param 
+            !! Current run configuration parameters
+
+        if (n < 0) return
+
+        call self%dealloc()
+        self%nbody = n
+
+        if (n == 0) return
+
+        allocate(self%active(n))
+        allocate(self%a(n))
+        allocate(self%mass(n))
+        allocate(self%Rhill(n))
+        allocate(self%rbin(n))
+        allocate(self%Torque(n))
+        allocate(self%Ttide(n))
+
+        self%active(:) = .false.
+        self%a(:) = 0.0_DP
+        self%mass(:) = 0.0_DP
+        self%Rhill(:) = 0.0_DP
+        self%rbin(:) = 0
+        self%Torque(:) = 0.0_DP
+        self%Ttide(:) = 0.0_DP
+
+        return
+    end subroutine ringmoons_util_setup_seeds
+
 end submodule s_ringmoons_util
