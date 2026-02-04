9e68c345d50bb155d448d38a27f0821c9c144a75
Kaustub P Anand <anand43@purdue.edu>
2026-02-04 13:44:02 -0500

    added yarkovsky effect to kicks and in swiftest_module

--- src/swiftest/swiftest_module.f90
+++ src/swiftest/swiftest_module.f90
@@ -400,6 +400,8 @@ module swiftest
          !! Compute the barycentric accelerations of bodies due to the oblateness of the central body
       procedure :: accel_radiation => swiftest_radiation_getacch_pl
       !    !! Compute the heliocentric accelerations of bodies due to radiation pressure and Poynting-Robertson drag
+      procedure :: accel_yarkovsky => swiftest_yarkovsky_getacch_pl
+         !! Compute the heliocentric accelerations of bodies due to the Yarkovsky effect
       procedure :: setup => swiftest_util_setup_pl          
          !! A base constructor that sets the number of bodies and allocates and initializes all arrays  
       ! procedure :: accel_tides    => tides_kick_getacch_pl           
@@ -1768,17 +1770,32 @@ module swiftest
    end interface
 
    interface 
-        module subroutine swiftest_radiation_getacch_pl(self, nbody_system, param)
-            implicit none
-            ! Arguments
-        class(swiftest_pl),         intent(inout) :: self
+      module subroutine swiftest_yarkovsky_getacch_pl(self, nbody_system, param)
+        !! author: Kaustub P. Anand and David A. Minton
+        !!
+        !! Calculate the Yarkovsky effect on massive bodies. 
+        !! Based on Ferich, et al, 2022 (https://iopscience.iop.org/article/10.3847/1538-4365/ac8d60) and Veras, et al, 2015 (https://academic.oup.com/mnras/article/451/3/2814/1180328)
+        implicit none
+        ! Arguments
+        class(swiftest_pl),           intent(inout) :: self
             !! Swiftest body object
         class(swiftest_nbody_system), intent(inout) :: nbody_system
             !! Swiftest nbody system object
         class(swiftest_parameters),   intent(in)    :: param
             !! Current run configuration parameters
+      end subroutine swiftest_yarkovsky_getacch_pl
 
-        end subroutine swiftest_radiation_getacch_pl
+      module subroutine swiftest_radiation_getacch_pl(self, nbody_system, param)
+         implicit none
+         ! Arguments
+      class(swiftest_pl),         intent(inout) :: self
+         !! Swiftest body object
+      class(swiftest_nbody_system), intent(inout) :: nbody_system
+         !! Swiftest nbody system object
+      class(swiftest_parameters),   intent(in)    :: param
+         !! Current run configuration parameters
+
+      end subroutine swiftest_radiation_getacch_pl
 
    end interface
 
