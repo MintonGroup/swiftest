0a415ce7b4e5274d3841067a5f736039fa315a38
Kaustub P Anand <anand43@purdue.edu>
2026-04-06 11:41:36 -0400

    wrote framework for yarkovsky test case

---
+++ tests/test_yarkovsky.py
@@ -0,0 +1,46 @@
+"""
+Copyright 2026 - David Minton.
+
+This file is part of Swiftest.
+Swiftest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Swiftest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Swiftest.
+If not, see: https://www.gnu.org/licenses.
+"""
+
+import os
+import tempfile
+import unittest
+
+import numpy as np
+
+import swiftest
+
+
+class TestCollisions(unittest.TestCase):
+    def setUp(self):
+        # Initialize a target and surface for testing
+        self.tmpdir = tempfile.TemporaryDirectory()
+        self.simdir = self.tmpdir.name
+
+    def tearDown(self):
+        # Clean up temporary directory
+        self.tmpdir.cleanup()
+
+    def test_yarkovsky(self):
+        """
+        Tests that Yarkovsky forces are handles correctly.
+        """
+
+        # check lack of inputted required variables
+        albedo = 0
+        
+    
+        return
+
+
+if __name__ == "__main__":
+    os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+    unittest.main()
