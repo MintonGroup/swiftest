c6c67e6ddcb4c09db95289c57901fd57ecdc9c48
David Minton <daminton@purdue.edu>
2026-04-15 07:34:30 -0400

    More adjustments to get the dependency build to happen and get more output for troubleshooting.

--- buildscripts/set_env_and_install.sh
+++ buildscripts/set_env_and_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Copyright 2026 - David Minton
+#
+# This file is part of Swiftest.
+# Swiftest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Swiftest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Swiftest. 
+# If not, see: https://www.gnu.org/licenses. 
+#
 # Sets environment variables for Linux or Mac. This script is designed so that the build system can be customized by the user simply
 # by defining any environment variables prior to running this script.
 # 
@@ -39,17 +49,12 @@
 # CC: The C compiler (defined for macOS only)
 # CXX: The C++ compiler (defined for macOS only)
 # 
-# Copyright 2025 - David Minton
-# This file is part of Swiftest.
-# Swiftest is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
-# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# Swiftest is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with Swiftest. 
-# If not, see: https://www.gnu.org/licenses. 
 
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+echo "Script directory: ${SCRIPT_DIR}"
+echo "Root directory: ${ROOT_DIR}"
 
 OS=$(uname -s)
 
@@ -156,7 +161,57 @@ elif [[ $OS == *"MINGW64"* ]]; then
     LD_LIBRARY_PATH="${PREFIX}/lib:${MINGW_PREFIX}/lib:${LD_LIBRARY_PATH}"
 fi
 
-sh ${SCRIPT_DIR}/build_dependencies.sh
+printenv
+
+echo "Checking for Ninja"
+if ! command -v ninja &> /dev/null; then
+    NINJA_VER="1.13.1"
+
+    printf "*********************************************************\n"
+    printf "*             FETCHING NINJA SOURCE                      *\n"
+    printf "*********************************************************\n"
+    printf "Copying files to ${DEPENDENCY_DIR}\n"
+    mkdir -p "${DEPENDENCY_DIR}"
+    if [ ! -d "${DEPENDENCY_DIR}"/ninja-${NINJA_VER} ]; then
+        [ -d "${DEPENDENCY_DIR}"/ninja-* ] && rm -rf "${DEPENDENCY_DIR}"/ninja-*
+        curl -L https://github.com/ninja-build/ninja/archive/refs/tags/v${NINJA_VER}.tar.gz | tar xvz -C "${DEPENDENCY_DIR}"
+    fi
+    cd "${DEPENDENCY_DIR}"/ninja-*
+    cmake -B build -S . -DCMAKE_INSTALL_PREFIX=/usr/local
+    cmake --build build 
+    if [ -w "${PREFIX}" ]; then
+        cmake --install build 
+    else
+        sudo cmake --install build
+    fi
+fi
+
+echo "Checking for OpenMP"
+
+# Get the OpenMP Libraries
+OS=$(uname -s)
+if [ $OS = "Darwin" ]; then
+    echo "Fetching OpenMP libraries for MacOS"
+    "${SCRIPT_DIR}"/get_lomp.sh 
+fi
+
+echo "Starting build scripts"
+
+"${SCRIPT_DIR}"/build_zlib.sh 
+"${SCRIPT_DIR}"/build_libaec.sh
+"${SCRIPT_DIR}"/build_bzip2.sh 
+"${SCRIPT_DIR}"/build_zstd.sh 
+"${SCRIPT_DIR}"/build_blosc.sh
+"${SCRIPT_DIR}"/build_hdf5.sh 
+"${SCRIPT_DIR}"/build_netcdf-c.sh
+"${SCRIPT_DIR}"/build_netcdf-fortran.sh
+"${SCRIPT_DIR}"/build_shtools.sh 
+
+printf "\n"
+printf "*********************************************************\n"
+printf "*             DEPENDENCIES ARE BUILT                    *\n"
+printf "*********************************************************\n"
+printf "Dependencys are installed to: ${PREFIX}\n\n"
 
 pip install .
 
