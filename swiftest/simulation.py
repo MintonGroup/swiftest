576edddfd6a403be2bb5c98a816711c0eb23ae71
David Minton <daminton@purdue.edu>
2026-04-16 11:34:47 -0400

    Added missing variables that did not survive the merge

--- swiftest/simulation.py
+++ swiftest/simulation.py
@@ -4397,6 +4397,10 @@ class Simulation:
             "ntp",
             "npl",
             "nplm",
+            "albedo",
+            "emissivity",
+            "rot_k",
+            "gamma",
         ]
 
         vars = [k for k in ic_vars if k in ds]
